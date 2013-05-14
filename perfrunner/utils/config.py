d1da9922779d0894f2bc711b1f874f7214473cb2
pavel-paulau <pavel.paulau@gmail.com>
2013-05-14 11:23:40 +0300

    create cluster spec parser

---
+++ perfrunner/utils/config.py
@@ -0,0 +1,39 @@
+import os.path
+from ConfigParser import ConfigParser, NoOptionError, NoSectionError
+from operator import add
+
+from perfrunner.logger import logger
+
+
+class ClusterSpec(object):
+
+    def parse(self, fname):
+        config = self._read_config(fname)
+        try:
+            self.clusters = tuple(
+                servers.split() for _, servers in config.items('clusters')
+            )
+            self.hosts = map(self.split_host_port, reduce(add, self.clusters))
+
+            self.data_path = config.get('storage', 'data')
+            self.index_path = config.get('storage', 'index')
+
+            self.rest_username, self.rest_password = \
+                config.get('credentials', 'rest').split(':')
+            self.ssh_username, self.ssh_password = \
+                config.get('credentials', 'ssh').split(':')
+        except (NoSectionError, NoOptionError), e:
+            logger.interrupt('Failed to get option from config: {0}'.format(e))
+
+    @staticmethod
+    def split_host_port(server):
+        return server.split(':')[0]
+
+    @staticmethod
+    def _read_config(fname):
+        logger.info('Reading configuration file: {0}'.format(fname))
+        if not os.path.isfile(fname):
+            logger.interrupt('File doesn\'t exist: {0}'.format(fname))
+        config = ConfigParser()
+        config.read(fname)
+        return config

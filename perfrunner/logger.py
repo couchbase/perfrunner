775ac4db33d7299ab038481cd2eb07aa9e4eebf9
pavel-paulau <pavel.paulau@gmail.com>
2013-05-14 11:23:05 +0300

    add log handler

---
+++ perfrunner/logger.py
@@ -0,0 +1,17 @@
+import logging
+import logging.config
+import os
+import sys
+import types
+
+config_file = os.path.join(os.path.dirname(__file__), 'logging.conf')
+
+logging.config.fileConfig(config_file)
+logger = logging.getLogger()
+
+
+def error(self, msg, *args, **kwargs):
+    self.error(msg, *args, **kwargs)
+    sys.exit(1)
+
+logger.interrupt = types.MethodType(error, logger)

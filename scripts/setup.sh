427e71a6a216650932b1edad195e034b5e9173ad
pavel-paulau <pavel.paulau@gmail.com>
2013-05-15 19:37:36 +0300

    cat cluster spec and test config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
+cat ${cluster}
+cat ${test_config}
+
 /tmp/prenv/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version}
 /tmp/prenv/bin/python -m perfrunner.utils.cluster -c ${cluster} -t ${test_config}

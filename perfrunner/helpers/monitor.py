2ba81931736d65a0f1750906347c010aa3f3fd2e
pavel-paulau <pavel.paulau@gmail.com>
2013-05-20 19:11:39 +0300

    implement tap replication monitor

--- perfrunner/helpers/monitor.py
+++ perfrunner/helpers/monitor.py
@@ -7,7 +7,16 @@ from perfrunner.helpers.rest import RestHelper
 
 class Monitor(RestHelper):
 
-    DISK_QUEUE_METRICS = ('ep_queue_size', 'ep_flusher_todo')
+    DISK_QUEUE_METRICS = (
+        'ep_queue_size',
+        'ep_flusher_todo',
+    )
+    TAP_REPLICATION_METRICS = (
+        'vb_replica_queue_size',
+        'ep_tap_replica_queue_itemondisk',
+        'ep_tap_rebalance_queue_backfillremaining',
+        'ep_tap_replica_qlen',
+    )
 
     def __init__(self, cluster_spec, test_config):
         super(RestHelper, self).__init__(cluster_spec, test_config)
@@ -24,16 +33,24 @@ class Monitor(RestHelper):
                 break
         logger.info('Rebalance successfully completed')
 
+    def _monitor_metric_value(self, host_port, bucket, metric):
+        while True:
+            bucket_stats = self.get_bucket_stats(host_port, bucket)
+            curr_value = bucket_stats['op']['samples'][metric][-1]
+            if curr_value:
+                logger.info('Current value of {0}: {1}'.format(metric,
+                                                               curr_value))
+                time.sleep(10)
+            else:
+                logger.info('{0} reached 0'.format(metric))
+                break
+
     def monitor_disk_queue(self, host_port, bucket):
         logger.info('Monitoring disk queue: {0}'.format(bucket))
         for metric in self.DISK_QUEUE_METRICS:
-            while True:
-                bucket_stats = self.get_bucket_stats(host_port, bucket)
-                curr_value = bucket_stats['op']['samples'][metric][-1]
-                if curr_value:
-                    logger.info('Current value of {0}: {1}'.format(metric,
-                                                                   curr_value))
-                    time.sleep(10)
-                else:
-                    logger.info('{0} reached 0'.format(metric))
-                    break
+            self._monitor_metric_value(host_port, bucket, metric)
+
+    def monitor_tap_replication(self, host_port, bucket):
+        logger.info('Monitoring TAP replication: {0}'.format(bucket))
+        for metric in self.TAP_REPLICATION_METRICS:
+            self._monitor_metric_value(host_port, bucket, metric)

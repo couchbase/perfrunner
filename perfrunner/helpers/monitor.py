0f2aaedd3899411c8f19c13241c91f795a851f4b
pavel-paulau <pavel.paulau@gmail.com>
2013-05-23 17:11:09 +0300

    unify _wait_for_nul_metric method

--- perfrunner/helpers/monitor.py
+++ perfrunner/helpers/monitor.py
@@ -32,7 +32,7 @@ class Monitor(RestHelper):
                 logger.info('Rebalance progress: {0} %'.format(progress))
         logger.info('Rebalance successfully completed')
 
-    def _monitor_metric_value(self, host_port, bucket, metric):
+    def _wait_for_nul_metric(self, host_port, bucket, metric):
         value = True
         while value:
             time.sleep(self.DELAY)
@@ -40,27 +40,21 @@ class Monitor(RestHelper):
             bucket_stats = self.get_bucket_stats(host_port, bucket)
             value = bucket_stats['op']['samples'][metric][-1]
 
-            logger.info('Current value of {0}: {1}'.format(metric, value))
+            if value:
+                logger.info('Current value of {0}: {1}'.format(metric, value))
         logger.info('{0} reached 0'.format(metric))
 
     def monitor_disk_queue(self, target):
         logger.info('Monitoring disk queue: {0}'.format(target.bucket))
         for metric in self.DISK_QUEUE_METRICS:
-            self._monitor_metric_value(target.node, target.bucket, metric)
+            self._wait_for_nul_metric(target.node, target.bucket, metric)
 
     def monitor_tap_replication(self, target):
         logger.info('Monitoring TAP replication: {0}'.format(target.bucket))
         for metric in self.TAP_REPLICATION_METRICS:
-            self._monitor_metric_value(target.node, target.bucket, metric)
+            self._wait_for_nul_metric(target.node, target.bucket, metric)
 
     def monitor_xdcr_replication(self, target):
         logger.info('Monitoring XDCR replication: {0}'.format(target.bucket))
-        changes_left = True
-        while changes_left:
-            time.sleep(self.DELAY)
-
-            stats = self.get_bucket_stats(target.node, target.bucket)
-            changes_left = stats['op']['samples']['replication_changes_left'][-1]
-
-            logger.info('Changes left: {1}'.format(changes_left))
-        logger.info('Replication finished')
+        metric = 'replication_changes_left'
+        self._wait_for_nul_metric(target.node, target.bucket, metric)

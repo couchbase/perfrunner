679f2d65e2c5c92d0cd36be7c2fae583625e39ea
pavel-paulau <pavel.paulau@gmail.com>
2013-06-26 23:16:47 +0300

    introduce experimental stats collection

--- perfrunner/tests/kv.py
+++ perfrunner/tests/kv.py
@@ -1,8 +1,18 @@
 from perfrunner.tests import PerfTest
 
 
+def with_stats(method):
+    def wrapper(self, *args, **kwargs):
+        self.cbagent.update_metadata()
+        self.cbagent.start()
+        method(self, *args, **kwargs)
+        self.cbagent.stop()
+    return wrapper
+
+
 class KVTest(PerfTest):
 
+    @with_stats
     def run(self):
         self._run_load_phase()
         self._compact_bucket()

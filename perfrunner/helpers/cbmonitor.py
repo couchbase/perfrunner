60be3235389b1bd63b80629eea77ad4b46c312e4
pavel-paulau <pavel.paulau@gmail.com>
2013-06-26 23:52:20 +0300

    use better cluster name

--- perfrunner/helpers/cbmonitor.py
+++ perfrunner/helpers/cbmonitor.py
@@ -10,7 +10,7 @@ class CbAgent(object):
 
     def __init__(self, cluster_spec, target_iterator):
         settings = CbAgentSettings()
-        settings.cluster = cluster_spec.name + uuid4().hex[:6]
+        settings.cluster = '{0}_{1}'.format(cluster_spec.name, uuid4().hex[:6])
         settings.ssh_username, settings.ssh_password = \
             cluster_spec.get_ssh_credentials()
         settings.rest_username, settings.rest_password = \

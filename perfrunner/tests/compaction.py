02ca6b3605b062af97ffdcaff4161dd5bfbd72b2
pavel-paulau <pavel.paulau@gmail.com>
2013-07-06 15:39:18 +0300

    skip bucket compaction in index compaction tests

--- perfrunner/tests/compaction.py
+++ perfrunner/tests/compaction.py
@@ -18,13 +18,11 @@ class IndexCompactionTest(IndexTest):
 
     def run(self):
         self._run_load_phase()
-        self._compact_bucket()
 
         self._define_ddocs()
         self._build_index()
 
         self._run_load_phase()  # extra mutations for index fragmentation
-        self._compact_bucket()
         self._build_index()
 
         self.reporter.start()

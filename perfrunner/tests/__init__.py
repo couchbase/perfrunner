91f659b0792890cd65986a596ac026426de47b48
pavel-paulau <pavel.paulau@gmail.com>
2013-08-03 14:12:24 +0300

    fix hot load phase

--- perfrunner/tests/__init__.py
+++ perfrunner/tests/__init__.py
@@ -80,7 +80,7 @@ class PerfTest(object):
     def hot_load(self):
         hot_load_settings = self.test_config.get_hot_load_settings()
         master_node = self.cluster_spec.get_masters().values()[0]
-        if self.rest.get_version(master_node) > '2.0.1':
+        if self.rest.get_version(master_node) >= '2.1.0':
             hot_load_settings.seq_updates = False
         logger.info('Running hot load phase: {0}'.format(hot_load_settings))
         self.worker_manager.run_workload(hot_load_settings, self.target_iterator)

d860dc978c5afcb808ef47f0ac1b9f7eb4f67af3
pavel-paulau <pavel.paulau@gmail.com>
2013-08-05 11:19:59 +0300

    fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,5 +2,9 @@
 cat ${cluster}
 cat ${test_config}
 
-/tmp/prenv/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version} -t ${toy}
+if [ -z "${toy}" ]; then
+    /tmp/prenv/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version}
+else
+    /tmp/prenv/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version} -t ${toy}
+fi
 /tmp/prenv/bin/python -m perfrunner.utils.cluster -c ${cluster} -t ${test_config}

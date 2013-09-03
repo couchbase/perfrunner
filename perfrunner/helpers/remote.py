6a263aaead4bebfca82c541e58ccb8e381321c7d
pavel-paulau <pavel.paulau@gmail.com>
2013-09-03 11:48:23 +0300

    use separate classes for linux and windows helpers

--- perfrunner/helpers/remote.py
+++ perfrunner/helpers/remote.py
@@ -22,7 +22,31 @@ class RemoteHelper(object):
 
     ARCH = {'i386': 'x86', 'x86_64': 'x86_64', 'unknown': 'x86_64'}
 
-    def __init__(self, cluster_spec):
+    def __new__(cls, cluster_spec):
+        os = cls.detect_os(cluster_spec)
+        if os == 'Cygwin':
+            return RemoteWindowsHelper(cluster_spec, os)
+        else:
+            return RemoteLinuxHelper(cluster_spec, os)
+
+    @staticmethod
+    def detect_os(cluster_spec):
+        logger.info('Detecting OS')
+        with settings(host_string=cluster_spec.get_all_hosts()[0]):
+            os = run('python -c "import platform; print platform.dist()[0]"',
+                     pty=False)
+        if os:
+            return os
+        else:
+            return 'Cygwin'
+
+
+class RemoteLinuxHelper(object):
+
+    ROOT_DIR = '/opt/couchbase'
+
+    def __init__(self, cluster_spec, os):
+        self.os = os
         self.hosts = cluster_spec.get_all_hosts()
         state.env.user, state.env.password = cluster_spec.get_ssh_credentials()
         state.output.running = False
@@ -42,11 +66,7 @@ class RemoteHelper(object):
     @single_host
     def detect_pkg(self):
         logger.info('Detecting package manager')
-        dist = run('python -c "import platform; print platform.dist()[0]"',
-                   pty=False)
-        if not dist:
-            return 'setup.exe'
-        elif dist in ('Ubuntu', 'Debian'):
+        if self.os in ('Ubuntu', 'Debian'):
             return 'deb'
         else:
             return 'rpm'
@@ -66,20 +86,51 @@ class RemoteHelper(object):
     @all_hosts
     def reset_swap(self):
         logger.info('Resetting swap')
-        run('swapoff --all && swapon --all', warn_only=True, quiet=True)
+        run('swapoff --all && swapon --all')
 
     @all_hosts
     def drop_caches(self):
         logger.info('Dropping memory cache')
-        run('sync && echo 3 > /proc/sys/vm/drop_caches',
-            warn_only=True, quiet=True)
+        run('sync && echo 3 > /proc/sys/vm/drop_caches')
 
     @all_hosts
     def collect_info(self):
         logger.info('Running cbcollect_info')
         fname = '/tmp/{0}.zip'.format(uuid4().hex)
-        r = run('/opt/couchbase/bin/cbcollect_info {0}'.format(fname),
-                warn_only=True, quiet=True)
+        r = run('{0}/bin/cbcollect_info {0}'.format(self.ROOT_DIR, fname),
+                warn_only=True)
+        if not r.return_code:
+            get('{0}'.format(fname))
+            run('rm -f {0}'.format(fname))
+
+
+class RemoteWindowsHelper(RemoteLinuxHelper):
+
+    ROOT_DIR = '/cygdrive/c/Program\ Files/Couchbase/Server'
+
+    @single_host
+    def detect_pkg(self):
+        logger.info('Detecting package manager')
+        return 'setup.exe'
+
+    @single_host
+    def detect_openssl(self, pkg):
+        pass
+
+    @all_hosts
+    def reset_swap(self):
+        pass
+
+    @all_hosts
+    def drop_caches(self):
+        pass
+
+    @all_hosts
+    def collect_info(self):
+        logger.info('Running cbcollect_info')
+        fname = '/tmp/{0}.zip'.format(uuid4().hex)
+        r = run('{0}/bin/cbcollect_info.exe {1}'.format(self.ROOT_DIR, fname),
+                warn_only=True)
         if not r.return_code:
             get('{0}'.format(fname))
             run('rm -f {0}'.format(fname))

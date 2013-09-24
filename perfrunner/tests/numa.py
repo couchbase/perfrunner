fb20bc1f90612acfc12b19ef83cc6293b4cc212c
ronniedada <ronniedada@gmail.com>
2013-09-24 11:22:11 -0700

    add numa specific test

---
+++ perfrunner/tests/numa.py
@@ -0,0 +1,18 @@
+from perfrunner.tests.kv import KVTest
+from perfrunner.helpers.cbmonitor import with_stats
+
+
+class NumaKVTest(KVTest):
+
+    @with_stats(latency=True, atop=True)
+    def access(self):
+        super(NumaKVTest, self).timer()
+
+    def run(self):
+        super(NumaKVTest, self).run()
+        for operation in ('get', 'set'):
+            self.reporter.post_to_sf(
+                *self.metric_helper.calc_kv_latency(operation=operation,
+                    percentile=0.9)
+            )
+

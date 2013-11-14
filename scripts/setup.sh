76b9f48fa15becb2c0cae3656351f9cd60667581
ronniedada <ronniedada@gmail.com>
2013-11-13 16:04:50 -0800

    use env variable to setup num of vbuckets

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@ cat ${cluster}
 cat ${test_config}
 
 if [ -z "${toy}" ]; then
-    /tmp/prenv${nickname}/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version} -b {vbuckets}
+    /tmp/prenv${nickname}/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version} -b ${vbuckets}
 else
-    /tmp/prenv${nickname}/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version} -t ${toy} -b {vbuckets}
+    /tmp/prenv${nickname}/bin/python -m perfrunner.utils.install -c ${cluster} -v ${version} -t ${toy} -b ${vbuckets}
 fi
 /tmp/prenv${nickname}/bin/python -m perfrunner.utils.cluster -c ${cluster} -t ${test_config}

c90262409b6e900bca880be70e8271a570b94599
pavel-paulau <pavel.paulau@gmail.com>
2013-12-07 12:31:03 -0800

    increase default failover timeout to 120 seconds

--- perfrunner/helpers/rest.py
+++ perfrunner/helpers/rest.py
@@ -243,7 +243,7 @@ class RestHelper(object):
         api = 'http://{}/diag/eval'.format(host_port)
         return self.post(url=api, data=cmd).text
 
-    def enable_auto_failover(self, host_port, timeout=45):
+    def enable_auto_failover(self, host_port, timeout=120):
         logger.info('Enabling auto-failover')
 
         api = 'http://{}/settings/autoFailover'.format(host_port)

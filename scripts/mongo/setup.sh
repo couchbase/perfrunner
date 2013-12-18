57d9dbc8f4ca0a8e6b5a2db88456a1074d71a270
ronniedada <ronniedada@gmail.com>
2013-12-17 17:36:15 -0800

    add flags to mongodb configuration process

--- scripts/mongo/setup.sh
+++ scripts/mongo/setup.sh
@@ -5,24 +5,28 @@ echo "cd /tmp; rm -fr perfrunner; git clone git://github.com/couchbaselabs/perfr
 echo "cd /tmp; rm -fr perfrunner; git clone git://github.com/couchbaselabs/perfrunner.git" | ssh root@node4 /bin/bash
 
 # UNINSTALL
+echo "Uninstall mongo"
 echo "cd /tmp/perfrunner/scripts/mongo; ./uninstall-mongo-local.sh" | ssh root@node1 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./uninstall-mongo-local.sh" | ssh root@node2 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./uninstall-mongo-local.sh" | ssh root@node3 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./uninstall-mongo-local.sh" | ssh root@node4 /bin/bash
 
 # INSTALL
+echo "Install mongo"
 echo "cd /tmp/perfrunner/scripts/mongo; ./install-mongo-local-4.sh" | ssh root@node1 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./install-mongo-local-4.sh" | ssh root@node2 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./install-mongo-local-4.sh" | ssh root@node3 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./install-mongo-local-4.sh" | ssh root@node4 /bin/bash
 
 # Start mongos
+echo "Start mongo"
 echo "cd /tmp/perfrunner/scripts/mongo; ./start-mongos-4.sh" | ssh root@node1 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./start-mongos-4.sh" | ssh root@node2 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./start-mongos-4.sh" | ssh root@node3 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./start-mongos-4.sh" | ssh root@node4 /bin/bash
 
 # REPLICA
+echo "Configure replicas"
 echo "cd /tmp/perfrunner/scripts/mongo; ./init-replica-4.sh" | ssh root@node1 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./init-replica-4.sh" | ssh root@node2 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./init-replica-4.sh" | ssh root@node3 /bin/bash
@@ -33,9 +37,11 @@ echo "sleeping 10 minutes"
 python -c "import time; time.sleep(10 * 60);"
 
 # SHARDING
+echo "Configure sharding"
 echo "cd /tmp/perfrunner/scripts/mongo; ./init-sharding-4.sh" | ssh root@node1 /bin/bash
 
 # INDEX
+echo "Configure index"
 echo "cd /tmp/perfrunner/scripts/mongo; ./init-index.sh" | ssh root@node1 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./init-index.sh" | ssh root@node2 /bin/bash
 echo "cd /tmp/perfrunner/scripts/mongo; ./init-index.sh" | ssh root@node3 /bin/bash

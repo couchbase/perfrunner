c7710daecd8d9e8fb49d955725e29f1a942b9667
Li Yang <li@couchbase.com>
2014-05-12 23:10:29 -0700

    Make user sync-gateway is really working before proceed to run sgw_test_info.sh and gateload
    Change-Id: I1496aa7fce43a9b4b9ec670b0e02e545153f37a6
    Reviewed-on: http://review.couchbase.org/37023
    Reviewed-by: Li Yang <lichenyang2@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- scripts/sgw_check_status.sh
+++ scripts/sgw_check_status.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-retry_count_max=5
+retry_count_max=20
 sleeptime_between_retry=3
 
 echo "Checking sync_gateway processes"
 retry_count=0
 while true; do
-    result=`ps aux | grep sync_gateway | grep -v grep`
+    result=`curl http://localhost:4985/db?n=1`
     if [ -z "$result" ]; then
         echo "Sync-gateway process is not running yet.  Retrying."
         retry_count=`expr $retry_count + 1`

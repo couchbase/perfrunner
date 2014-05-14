8a1b71f95138e240484cd078a17587d2c2c7fc9f
Li Yang <li@couchbase.com>
2014-05-13 17:23:05 -0700

    Add ip address to the retry message and double the maximum retry counts
    Change-Id: Icf7b28a8eff9c1c4b06da63cbe546aba68bab21a
    Reviewed-on: http://review.couchbase.org/37053
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Li Yang <lichenyang2@gmail.com>

--- scripts/sgw_check_status.sh
+++ scripts/sgw_check_status.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-retry_count_max=20
+retry_count_max=40
 sleeptime_between_retry=3
+myip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
 
 echo "Checking sync_gateway processes"
 retry_count=0
@@ -11,7 +12,7 @@ while true; do
         echo "Sync-gateway process is not running yet.  Retrying."
         retry_count=`expr $retry_count + 1`
         if [ $retry_count -gt $retry_count_max ]; then
-          echo "Sync-gateway process is not running yet after $retry_count_max retries.   Giving up."
+          echo "Sync-gateway process is not running on ${myip} yet after $retry_count_max retries.   Giving up."
           exit 9
         fi
         sleep $sleeptime_between_retry

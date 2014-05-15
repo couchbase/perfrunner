f3c5a7d6f14c907f7485fddfeeec7c28816596d3
Li Yang <li@couchbase.com>
2014-05-15 16:53:23 -0700

    Calculate average for p99 and p95 and compare to the criteria
    Change-Id: I2f48fae913a1db6d86f4173c89b3a80bb6da848e
    Reviewed-on: http://review.couchbase.org/37157
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Li Yang <lichenyang2@gmail.com>

--- perfrunner/tests/sync_gateway.py
+++ perfrunner/tests/sync_gateway.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 import json
 import time
 
+import numpy as np
 from logger import logger
 from seriesly import Seriesly
 
@@ -53,12 +55,26 @@ class SyncGatewayGateloadTest(PerfTest):
             with open(fname, 'w') as fh:
                 fh.write(pretty_dict(expvar))
 
+        latencies = defaultdict(list)
         for idx, gateload in enumerate(self.cluster_spec.gateloads, start=1):
             logger.info('Test results for {} ({}):'.format(gateload, idx))
             for p in (95, 99):
                 latency = self.metric_helper.calc_push_latency(p=p, idx=idx)
+                latencies[p].append(latency)
                 logger.info('\tPushToSubscriberInteractive/p{} average: {}'
                             .format(p, latency))
+        criteria = {
+            95: self.test_config.gateload_settings.p95_avg_criteria,
+            99: self.test_config.gateload_settings.p99_avg_criteria
+        }
+        for p in (95, 99):
+            average = np.mean(latencies[p])
+            if average > criteria[p]:
+                logger.info('\tPushToSubscriberInteractive/p{} average: {} - does not meet the criteria of {}'
+                            .format(p, average, criteria[p]))
+            else:
+                logger.warn('\tPushToSubscriberInteractive/p{} average: {} - meet the criteria of {}'
+                            .format(p, average, criteria[p]))
 
     @with_stats
     def workload(self):

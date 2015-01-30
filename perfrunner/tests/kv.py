f2377dfd2487666ec31f90ea9d89bf6800e9d2a6
Venu Uppalapati <venumahesh@gmail.com>
2015-01-30 10:24:48 -0800

    resolve circular import conflict
    Change-Id: Id798888fb5b029687730e3ca5dd1b68c9d99490f
    Reviewed-on: http://review.couchbase.org/46143
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Venu Uppalapati <venumahesh@gmail.com>
    Tested-by: Venu Uppalapati <venumahesh@gmail.com>

--- perfrunner/tests/kv.py
+++ perfrunner/tests/kv.py
@@ -8,8 +8,7 @@ from couchbase.user_constants import OBS_NOTFOUND
 from logger import logger
 from mc_bin_client.mc_bin_client import MemcachedClient, MemcachedError
 from tap import TAP
-from dcp import DcpClient
-from dcp.constants import CMD_STREAM_REQ, SUCCESS
+import dcp
 
 from perfrunner.helpers.cbmonitor import with_stats
 from perfrunner.helpers.misc import pretty_dict, uhex

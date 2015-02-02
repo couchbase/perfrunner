b6089aee176f315042bc455f5eccdd6b39e381dd
Li Yang <li@couchbase.com>
2015-02-02 14:36:27 -0800

    Fix a typo on output file name
    Change-Id: Iabbf15cac2a778ee02b0f730284eb6194c1c3261
    Reviewed-on: http://review.couchbase.org/46278
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Venu Uppalapati <venumahesh@gmail.com>
    Reviewed-by: Traun Leyden <tleyden@couchbase.com>
    Tested-by: Traun Leyden <tleyden@couchbase.com>

--- perfrunner/helpers/remote.py
+++ perfrunner/helpers/remote.py
@@ -398,7 +398,7 @@ class RemoteLinuxHelper(object):
         get('sgw_test_info.txt', 'sgw_test_info_{}.txt'.format(index))
         get('gateway_config.json', 'gateway_config_{}.json'.format(index))
         get('sgw_check_logs.out', 'sgw_check_logs_gateway_{}.out'.format(index))
-        get('gateload_expvar_{}.json'.format(index + 1), 'gateload_expvar_{}.out'.format(index + 1))
+        get('gateload_expvar_{}.json'.format(index + 1), 'gateload_expvar_{}.json'.format(index + 1))
 
         if self.test_config.gateway_settings.profiling_freq != '0':
             get('profiling.tar.gz', 'profiling_{}.tar.gz'.format(index))

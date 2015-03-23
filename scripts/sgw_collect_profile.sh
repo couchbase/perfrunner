9e69f935aa109982895c7ff683ad4f22d2fd4685
Traun Leyden <tleyden@couchbase.com>
2015-03-23 15:33:54 -0700

    Collect raw profiles for later analysis
    * Collect raw profiles for later analysis in addition to rendered profiles
    * Add text format in addition to pdf format
    * Save all files to temp dir
    * Delete temp dir at the end
    
    https://github.com/couchbase/sync_gateway/issues/715
    
    Change-Id: Icea6e7ec8b8453ffcfb3b244e0a612868d58a86a
    Reviewed-on: http://review.couchbase.org/48654
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- scripts/sgw_collect_profile.sh
+++ scripts/sgw_collect_profile.sh
@@ -2,8 +2,7 @@
 
 ## Collect profile data as PDFs and create a .tar.gz file
 
-import os, glob, re, shutil, sys, re, subprocess
-
+import os, shutil, sys, tempfile
 
 profile_types = [
     "profile",
@@ -11,8 +10,10 @@ profile_types = [
     "goroutine",
 ]
 
-# keep track of all the profile pdfs that are generated so we can clean up later
-profile_pdfs = []
+format_types = [
+    "pdf",
+    "text",
+]
 
 sg_pprof_url = "http://localhost:4985/_debug/pprof"
 
@@ -22,40 +23,60 @@ def run_command(command):
         raise Exception("{0} failed".format(command))
 
 def collect_profiles(tmp_dir, sg_binary_path):
+
+    # make sure raw profile gz files end up in tmp dir 
+    os.environ["PPROF_TMPDIR"] = tmp_dir
+
     for profile_type in profile_types:
-        print "Collecting profile for {0}".format(profile_type)
-        pdf_filename = "{0}.pdf".format(profile_type)
-        dest_path = os.path.join(tmp_dir, pdf_filename)
-        profile_pdfs.append(dest_path)
-        cmd = "go tool pprof -pdf {0} {1}/{2} > {3}".format(
-            sg_binary_path,
-            sg_pprof_url,
-            profile_type,
-            dest_path,
-        )
-        print cmd
-        run_command(cmd)
+        for format_type in format_types:
+            print "Collecting {0} profile in format {1}".format(profile_type, format_type)
+            out_filename = "{0}.{1}".format(profile_type, format_type)
+            dest_path = os.path.join(tmp_dir, out_filename)
+            cmd = "go tool pprof -{0} {1} {2}/{3} > {4}".format(
+                format_type,
+                sg_binary_path,
+                sg_pprof_url,
+                profile_type,
+                dest_path,
+            )
+            print cmd
+            run_command(cmd)
 
 
 def create_tar_gz(tmp_dir):
-    dest_file = os.path.join(tmp_dir, "profile_data.tar.gz")
-    run_command("tar cvfz {0} {1}/*.pdf".format(dest_file, tmp_dir))
+    dest_file = result_file(tmp_dir)
+    cwd = os.getcwd()
+    os.chdir(tmp_dir)
+    run_command("tar cvfz {0} *".format(dest_file))
+    os.chdir(cwd)
 
-def cleanup_files(tmp_dir):
-    for profile_pdf in profile_pdfs:
-        os.remove(profile_pdf)
+def result_file(tmp_dir):
+    return os.path.join(tmp_dir, "profile_data.tar.gz")
+
+def copy_result(tmp_dir, dest_dir):
+    src_file = result_file(tmp_dir)
+    dest_file = os.path.join(dest_dir, "profile_data.tar.gz")
+    shutil.copyfile(src_file, dest_file)
 
 if __name__ == "__main__":
 
     # get commit from command line args and validate
     if len(sys.argv) <= 2:
-        raise Exception("Usage: {0} <path-sync-gw-binary> <tmp-dir>".format(sys.argv[0]))
+        raise Exception("Usage: {0} <path-sync-gw-binary> <dest-dir>".format(sys.argv[0]))
 
     sg_binary_path = sys.argv[1]
-    tmp_dir = sys.argv[2]
 
+    # this is where the profile_data.tar.gz result will be stored
+    dest_dir = sys.argv[2]
+
+    # this is the temp dir where collected files will be stored.  will be deleted at end.
+    tmp_dir = tempfile.mkdtemp()
+    
     collect_profiles(tmp_dir, sg_binary_path)
 
     create_tar_gz(tmp_dir)
 
-    cleanup_files(tmp_dir)
+    copy_result(tmp_dir, dest_dir)
+
+    # delete the tmp dir since we're done with it
+    shutil.rmtree(tmp_dir) 

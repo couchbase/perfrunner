30ba80348fb282604ceaf284ac7d1096b41b492f
Mike Wiederhold <mikewied06@gmail.com>
2015-04-08 16:19:15 -0700

    Update the dcp test strategy
    Change-Id: I88d91f87b86d09a5e795ceef75978ab28345d325
    Reviewed-on: http://review.couchbase.org/49681
    Reviewed-by: thomas anderson <thomas.couchbase@yahoo.com>
    Tested-by: thomas anderson <thomas.couchbase@yahoo.com>

--- perfrunner/tests/dcp.py
+++ perfrunner/tests/dcp.py
@@ -1,11 +1,23 @@
 
 from perfrunner.tests import PerfTest
+from perfrunner.helpers.cbmonitor import with_stats
 
+class ReplicationTest(PerfTest):
 
-class SingleNodeTest(PerfTest):
+    COLLECTORS = {'replicate_latency': True}
+
+    @with_stats
+    def access(self):
+        super(ReplicationTest, self).timer()
 
     def run(self):
         self.load()
+        self.wait_for_persistence()
+
+        self.compact_bucket()
+
+        self.hot_load()
 
         self.workload = self.test_config.access_settings
+        self.access_bg()
         self.access()

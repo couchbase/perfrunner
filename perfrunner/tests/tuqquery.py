7ecf914811faed61f93ea68f80573cc25ba39c97
Mike Wiederhold <mikewied06@gmail.com>
2015-04-14 16:47:38 -0700

    Add the tuqquery test
    Change-Id: Id58d85dff730b15536ef7f5ad7f29fbb6cb05d14
    Reviewed-on: http://review.couchbase.org/49717
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: thomas anderson <thomas.couchbase@yahoo.com>
    Tested-by: thomas anderson <thomas.couchbase@yahoo.com>

---
+++ perfrunner/tests/tuqquery.py
@@ -0,0 +1,41 @@
+from perfrunner.helpers.cbmonitor import with_stats
+from perfrunner.tests import PerfTest, TargetIterator
+from perfrunner.workloads.n1ql import INDEX_STATEMENTS
+from logger import logger
+
+
+class TuqTest(PerfTest):
+
+    COLLECTORS = {'n1ql_latency': True}
+
+    def __init__(self, *args, **kwargs):
+        super(TuqTest, self).__init__(*args, **kwargs)
+        self.n1ql = True
+        self.target_iterator = TargetIterator(self.cluster_spec,
+                                              self.test_config,
+                                              prefix='')
+
+    def build_index(self):
+        statements = INDEX_STATEMENTS[
+            self.test_config.index_settings.index_type
+        ]
+        for master in self.cluster_spec.yield_masters():
+            for bucket in self.test_config.buckets:
+                for statement in statements:
+                    host = master.split(':')[0]
+                    self.rest.exec_n1ql_stmnt(host, statement.format(bucket))
+
+    @with_stats
+    def access(self):
+        super(TuqTest, self).timer()
+
+    def run(self):
+        self.load()
+        self.wait_for_persistence()
+        self.compact_bucket()
+
+        self.build_index()
+
+        self.workload = self.test_config.access_settings
+        self.access_bg()
+        self.access()

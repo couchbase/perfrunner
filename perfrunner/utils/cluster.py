9eb99faefd85ee6640fa9d6caebec11f89aedaa1
Venu Uppalapati <venumahesh@gmail.com>
2015-05-12 18:45:51 -0700

    increase time delay before internal settings change
    Change-Id: Ia0efa9b7f4e72b86765927e55bdbefe3f824a424
    Reviewed-on: http://review.couchbase.org/50943
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Wayne Siu <wayne@couchbase.com>

--- perfrunner/utils/cluster.py
+++ perfrunner/utils/cluster.py
@@ -238,7 +238,7 @@ def main():
         cm.restart_with_alternative_num_cpus()
         cm.restart_with_tcmalloc_aggressive_decommit()
         cm.disable_moxi()
-    time.sleep(30)  # crutch
+    time.sleep(300)  # crutch
     cm.configure_internal_settings()
     cm.set_data_path()
     cm.set_services()

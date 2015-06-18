f8297149d1e2a06505d4b0a4600e6d2da3dce1f6
Mike Wiederhold <mikewied06@gmail.com>
2015-06-18 10:29:11 -0700

    Increase the number of file descriptors when running spring jobs
    Change-Id: I7f9a481fd355a72b106a01e367a39a9c8bf1d3ec
    Reviewed-on: http://review.couchbase.org/52257
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: thomas anderson <thomas@couchbase.com>

--- perfrunner/helpers/worker.py
+++ perfrunner/helpers/worker.py
@@ -106,7 +106,7 @@ class RemoteWorkerManager(object):
                 logger.info('Starting remote Celery worker: {}'.format(qname))
 
                 temp_dir = '{}-{}/perfrunner'.format(self.temp_dir, qname)
-                run('cd {0}; nohup env/bin/celery worker '
+                run('cd {0}; ulimit -n 10240; nohup env/bin/celery worker '
                     '-A perfrunner.helpers.worker -Q {1} -c 1 '
                     '&>/tmp/worker_{1}.log &'.format(temp_dir, qname),
                     pty=False)

26f07e96efcff080b785cc212bf84bdde0938832
Venu Uppalapati <venumahesh@gmail.com>
2015-06-29 13:51:48 -0700

    handle the case when override is not provided
    Change-Id: If343ee3c5df0ade3ef284fd4fe9a8fdbaf32bb26
    Reviewed-on: http://review.couchbase.org/52678
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Venu Uppalapati <venumahesh@gmail.com>

--- scripts/workload.sh
+++ scripts/workload.sh
@@ -5,4 +5,8 @@ then
 	ENV_FOLDER="/tmp"
 fi
 
-PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} ${override} stats.post_to_sf.1
+if [ -z "${override}" ]; then
+    PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} stats.post_to_sf.1
+else
+    PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} ${override},stats.post_to_sf.1
+fi

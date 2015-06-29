b1ff8594071293de9679a760b3415636f47d1fde
Venu Uppalapati <venumahesh@gmail.com>
2015-06-29 11:04:16 -0700

    add override param to workload.sh
    Change-Id: Ib93ad595aaa80865948b77e0778869cde29566a6
    Reviewed-on: http://review.couchbase.org/52666
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Venu Uppalapati <venumahesh@gmail.com>

--- scripts/workload.sh
+++ scripts/workload.sh
@@ -5,4 +5,4 @@ then
 	ENV_FOLDER="/tmp"
 fi
 
-PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} stats.post_to_sf.1
+PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} ${override} stats.post_to_sf.1

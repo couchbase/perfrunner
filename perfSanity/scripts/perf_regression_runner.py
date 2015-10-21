5f098853219f8da061026ed0288e60c1bf780b69
Eric <ericcouchbase@gmail.com>
2015-10-21 09:30:40 -0700

    CBQE-3178 Archive results to couchbase, also improve the output readability
    Change-Id: Ie45f2ccae21f8958bcad1ee55f95c6698d4711e5
    Reviewed-on: http://review.couchbase.org/56293
    Reviewed-by: Eric Cooper <ericcouchbase@gmail.com>
    Tested-by: Eric Cooper <ericcouchbase@gmail.com>

--- perfSanity/scripts/perf_regression_runner.py
+++ perfSanity/scripts/perf_regression_runner.py
@@ -9,6 +9,10 @@ import os
 import sys
 import time
 
+from couchbase.bucket import Bucket
+import couchbase
+
+
 """
 
 # An evolving thing - takes as input:
@@ -21,6 +25,33 @@ This program parses out the results from the files and compares them against the
 """
 
 
+test_workload_output = '''
+[20/Oct/2015 15:01:26] INFO - Creating new database: iostatperfregression_410-4859-enterprise_27b10170106
+[20/Oct/2015 15:01:29] INFO - Creating new database: ns_serverperfregression_410-4859-enterprise_27bbucket-110170107
+[20/Oct/2015 15:02:08] INFO - Adding snapshot: perfregression_410-4859-enterprise_27b_access
+[20/Oct/2015 15:02:15] INFO - http://cbmonitor.sc.couchbase.com/reports/html/?snapshot=perfregression_410-4859-enterprise_27b_access
+[20/Oct/2015 15:03:04] INFO - http://cbmonitor.sc.couchbase.com/reports/get_corr_matrix/?snapshot=perfregression_410-4859-enterprise_27b_access
+[20/Oct/2015 15:03:31] INFO - Dry run stats: {
+    "build": "4.1.0-4859-enterprise", 
+    "build_url": null, 
+    "metric": "perf_sanity_kv_latency_mixed_2M_short_get_95th_perf_sanity_base_test", 
+    "snapshots": [
+        "perfregression_410-4859-enterprise_27b_access"
+    ], 
+    "value": 0.56
+}
+[20/Oct/2015 15:03:31] INFO - Dry run stats: {
+    "build": "4.1.0-4859-enterprise", 
+    "build_url": null, 
+    "metric": "perf_sanity_kv_latency_mixed_2M_short_set_95th_perf_sanity_base_test", 
+    "snapshots": [
+        "perfregression_410-4859-enterprise_27b_access"
+    ], 
+    "value": 0.95
+}
+[20/Oct/2015 15:03:31] INFO - Terminating local Celery workers
+'''
+
 def main():
     print 'Starting the perf regression runner'
 
@@ -29,10 +60,12 @@ def main():
 
     parser.add_option('-f', '--filename', dest='filename')
     parser.add_option('-v', '--version', dest='version')
+    parser.add_option('-s', '--summaryFile', dest='summaryFile')
 
     options, args = parser.parse_args()
     summary = []
 
+    archive_bucket = Bucket('couchbase://172.23.105.177/QE-Performance-Sanity')
     for line in fileinput.input(options.filename):
 
         time.sleep(10)
@@ -42,7 +75,7 @@ def main():
 
         test, spec, params = line.split()
         print '\n\n', time.asctime( time.localtime(time.time()) ), 'Now running', test
-        current_summary = {'test': test, 'output': '', 'result': True}
+        current_summary = {'test': test, 'status':'run', 'results':[]}
 
         test = 'perfSanity/tests/' + test
         spec = 'perfSanity/clusters/' + spec
@@ -52,6 +85,8 @@ def main():
         my_env['test_config'] = test
         my_env['version'] = options.version
         # proc = subprocess.Popen('ls', env=my_env, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+        #"""
         proc = subprocess.Popen('./scripts/setup.sh', env=my_env, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 shell=True)
 
@@ -65,8 +100,8 @@ def main():
             print '\n\nHave an error during setup'
             print stderrdata
             print stdoutdata
-            current_summary['output'] += '  Have an error during setup'
-            current_summary['result'] = False
+            current_summary['output'] = '  Have an error during setup'
+            current_summary['status'] = 'not run'
             summary.append(current_summary)
             continue
 
@@ -84,16 +119,17 @@ def main():
 
         if proc.returncode == 1:
             print '  Have an error during workload generation'
-            current_summary['output'] += '  Have an error during workload generation'
+            current_summary['output'] = '  Have an error during workload generation'
             sys.stdout.flush()
-            current_summary['result'] = False
+            current_summary['status'] = 'not run'
             print stderrdata
             summary.append(current_summary)
             continue
 
         print '\n\nWorkload complete, analyzing results'
+        #"""
         # parse the line for actual values
-        # workload_output = test_workload_output
+        #workload_output = test_workload_output
         p = re.compile(r'Dry run stats: {(.*?)}', re.MULTILINE)
         matches = p.findall(workload_output.replace('\n', ''))
 
@@ -106,55 +142,65 @@ def main():
         expected_keys = json.loads(params)
         for k in expected_keys.keys():
             if k in actual_values:
+                # archive the value, the key is a concatenation of time, version and metric
+                archive_bucket.upsert( time.asctime( time.localtime(time.time()) ) + '-' + options.version + '-' + k,
+                    actual_values[k], format=couchbase.FMT_JSON)
+
+
+                current_summary['results'].append( {'metric':k, 'expected':expected_keys[k], 'actual':actual_values[k][ 'value']})
                 if actual_values[k]['value'] > 1.05 * expected_keys[k]:
                     print '  ', k, ' is greater than expected. Expected', expected_keys[k], 'Actual', actual_values[k][
                         'value']
-                    current_summary['output'] += '\n    {0} is greater than expected. Expected {1} actual {2}'.format(k,
-                                                                                                                      expected_keys[
-                                                                                                                          k],
-                                                                                                                      actual_values[
-                                                                                                                          k][
-                                                                                                                          'value'])
-                    current_summary['result'] = False
 
                 elif actual_values[k]['value'] < 0.95 * expected_keys[k]:
                     # sort of want to yellow flag this but for now all we have is a red flag so use that
                     print '  ', k, ' is less than expected. Expected', expected_keys[k], 'Actual', actual_values[k][
                         'value']
-                    current_summary['output'] += '\n    {0} is less than expected. Expected {1} actual {2}'.format(k,
-                                                                                                                   expected_keys[
-                                                                                                                       k],
-                                                                                                                   actual_values[
-                                                                                                                       k][
-                                                                                                                       'value'])
-                    current_summary['result'] = False
 
                 del actual_values[k]
             else:
+                current_summary['results'].append( {'metric':k, 'expected':'Not found' , 'actual':k})
                 print '  Expected key', k, ' is not found'
-                current_summary['output'] += '\n    Expected key {0} is not found'.format(k)
-                current_summary['result'] = False
+                #current_summary['output'] += '\n    Expected key {0} is not found'.format(k)
+
         if len(actual_values) > 0:
             print '  The following key(s) were present but not expected:'
             for i in actual_values:
                 print '    ', i
-                current_summary['output'] += '\n    {0} is present but not expected'.format(i)
-                current_summary['result'] = False
+                current_summary['results'].append( {'metric':k, 'expected':i , 'actual':'Not found'})
 
         summary.append(current_summary)
         print '\nCompleted analysis for', test
         time.sleep(10)
     # end the for loop - print the results
-    all_success = True
-    print '\n\nResult summary:'
-    for i in summary:
-        print '\n', i['test'],
-        if i['result']:
-            print '  ...passed'
-        else:
-            print i['output']
-            all_success = False
 
+    all_success = True
+    fo = open(options.summaryFile, "wb")
+
+    print '\n\nTest\t\t\t\t\tMetric\t\t\t\t\tActual\t\t\t\t\tExpected'
+    for i in summary:
+        #fo.write( '\n' + i['test'])
+        if i['status'] == 'not run':
+           print i['test'], 'not run:', i['output']
+           all_success = False
+        else:
+            for j in i['results']:
+               print i['test'], '\t', j['metric'], '\t', j['actual'], '\t', j['expected'],
+               if j['actual'] == 'Not found' or j['expected'] == 'Not found':
+                  all_success = False
+               else:
+                  # do the compares
+                  if j['actual']  > 1.1 * j['expected']:
+                      print '\tactual is too large',
+                      all_success = False
+                  elif j['actual']  < 0.9 * j['expected']:
+                      print '\tactual is too small',
+                      all_success = False
+               print
+        print # blank line at end of test
+
+    #endfor all tests
+    fo.close()
     return all_success
 
 

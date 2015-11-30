a288af3d4d25d3588937a3798721683908b95971
sandipnd <sandip.nandi@couchbase.com>
2015-11-30 14:10:18 -0800

    CBQE:3138 more tuning to result format
    Change-Id: I2696c03bdc18cbbbb3b0b841dd942662443432d6
    Reviewed-on: http://review.couchbase.org/57336
    Reviewed-by: sandip nandi <sandip.nandi@couchbase.com>
    Tested-by: sandip nandi <sandip.nandi@couchbase.com>

--- perfSanity/scripts/perf_regression_runner_new.py
+++ perfSanity/scripts/perf_regression_runner_new.py
@@ -70,18 +70,18 @@ def cb_data_analysis(actual_values,test_name,variation,params,analysis_data):
       for k in params.keys():
            if actual_values[k]['value'] > (upper_variation * params[k]):
                 print '  ', test_name, ' is greater than expected. Expected value for key ',k, ' ', params[k], ' Actual ', actual_values[k]['value'],'\n'
-                temp_analysis_data.append(' expected result: '+str(params[k])+' :Result greater than expected , test result:=> '+ str(actual_values[k]['value']))
+                temp_analysis_data.append(str('Fail: result is greater than expected: '+str(params[k])))
                 result *=False
            elif actual_values[k]['value'] < (lower_variation * params[k]):
                 # sort of want to yellow flag this but for now all we have is a red flag so use that
                 print '  ', test_name, ' is less than expected. Expected for key ',k, ' ', params[k], 'Actual ', actual_values[k]['value'],'\n'
 
-                temp_analysis_data.append(' expected result: '+str(params[k])+' :Result less than expected , test result:=> '+ str(actual_values[k]['value']))
+                temp_analysis_data.append(str('Fail: result is lower than expected: '+str(params[k])))
                 result *= False
            else:
              result *= True
              print test_name ,'  ' ,params[k],  ' result is expected'
-             temp_analysis_data.append(' expected result: '+str(params[k])+' :Result is expected value :=> ' + str(actual_values[k]['value']))
+             temp_analysis_data.append(str('Pass : result is expected'+str(params[k])))
       analysis_data.append(temp_analysis_data)
 
       return result

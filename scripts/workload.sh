6188a7e2fa356680d5eb90e5715ed2284594a619
Volker Mische <volker.mische@gmail.com>
2016-03-30 09:42:08 +0200

    Don't always post to ShowFast
    If an override is given, don't automatically always post to ShowFast.
    
    Change-Id: Iaa80fffc403210dc62ae5bf71c04bdfea327020b
    Reviewed-on: http://review.couchbase.org/62148
    Reviewed-by: Wayne Siu <wayne@couchbase.com>
    Tested-by: Volker Mische <volker.mische@gmail.com>

--- scripts/workload.sh
+++ scripts/workload.sh
@@ -8,5 +8,5 @@ fi
 if [ -z "${override}" ]; then
     PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} stats.post_to_sf.1
 else
-    PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} ${override},stats.post_to_sf.1
+    PYCBC_ASSERT_CONTINUE=1 $ENV_FOLDER/env/bin/python -m perfrunner -c ${cluster} -t ${test_config} ${override}
 fi

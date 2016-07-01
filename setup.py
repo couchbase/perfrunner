2c62c38ad44be31cbb87a76d57db9cfbe2384c34
Pavel Paulau <pavel.paulau@gmail.com>
2016-07-01 16:24:23 -0700

    Add perfrunner console scripts
    Change-Id: Id10e6a768246d62226d9ab9eb243fa533af999f2
    Reviewed-on: http://review.couchbase.org/65456
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
         'console_scripts': [
             'cbagent = cbagent.__main__:main',
             'spring = spring.__main__:main',
+            'perfrunner = perfrunner.__main__:main',
+            'install = perfrunner.utils.install:main',
+            'cluster = perfrunner.utils.cluster:main',
         ]
     },
     ext_modules=[

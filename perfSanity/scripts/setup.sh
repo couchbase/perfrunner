eed67b2730b7f013b33848e278c680bc201ade91
Eric <ericcouchbase@gmail.com>
2016-07-10 09:56:09 -0700

    CBPS-0:Daily sanity with a toy build
    Change-Id: I8b0175697c82f174c3ae5822da4a3486c83fa5ea
    Reviewed-on: http://review.couchbase.org/65644
    Reviewed-by: Eric Cooper <ericcouchbase@gmail.com>
    Tested-by: Eric Cooper <ericcouchbase@gmail.com>

--- perfSanity/scripts/setup.sh
+++ perfSanity/scripts/setup.sh
@@ -10,7 +10,8 @@ fi
 make
 if [ -z "${toy}" ]; then
     if [ -z "${url}" ]; then
-        env/bin/install -c ${cluster} -v ${version}
+        env/bin/install -c ${cluster} --url=http://latestbuilds.hq.couchbase.com/couchbase-server/toy-sriram/32/couchbase-server-enterprise-4.7.0-4-centos6.x86_64.rpm -v 4.7.0-004
+        #env/bin/install -c ${cluster} -v ${version}
     else
         env/bin/install -c ${cluster} --url ${url}
     fi

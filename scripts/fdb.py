a14c5c98b60ffa3fdd3fd62588a5a1b9a05e59e8
Pavel Paulau <pavel.paulau@gmail.com>
2016-08-05 13:23:39 -0700

    Add more details to FDB summary
    Change-Id: I6f4269f6db73186cb0813b638f22c57e9569e540
    Reviewed-on: http://review.couchbase.org/66552
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/fdb.py
+++ scripts/fdb.py
@@ -6,20 +6,61 @@ import requests
 
 
 benchmark = {
-    'group': 'ForestDB, {} workload',
+    'group': 'ForestDB',
     'metric': None,
     'value': None,
 }
 
 
-def set_title(line):
+def set_documents(line):
+    """Example:
+        # documents (i.e. working set size): 80000000
+    """
+    num_docs = int(line.split()[6])
+    benchmark['group'] += ': {}M docs'.format(num_docs / 10 ** 6)
+
+
+def set_workload(line):
+    """Example:
+        write ratio: 80 % (synchronous)
+    """
     ratio = int(line.split()[2])
     if ratio < 50:
-        benchmark['group'] = benchmark['group'].format('Read-heavy')
+        benchmark['group'] += ', read-heavy'
     if ratio > 50:
-        benchmark['group'] = benchmark['group'].format('Write-heavy')
+        benchmark['group'] += ', write-heavy'
     else:
-        benchmark['group'] = benchmark['group'].format('Mixed')
+        benchmark['group'] += ', mixed'
+
+
+def set_compaction(line):
+    """Example: compaction threshold: 100 % (period: 60 seconds, auto)"""
+    threshold = int(line.split()[2])
+    benchmark['group'] += ', {}% compaction threshold'.format(threshold)
+
+
+def set_cbr(line):
+    """Example:
+        block reuse threshold: 65 %
+    """
+    threshold = int(line.split()[3])
+    benchmark['group'] += ', {}% CBR threshold'.format(threshold)
+
+
+def set_throughput(line):
+    """Example:
+        34858349 reads (9682.83 ops/sec, 826.20 us/read)
+        139437362 writes (38732.42 ops/sec, 103.27 us/write)
+    """
+    if 'writes' in line:
+        benchmark['metric'] = 'Write throughput, ops/sec'
+    elif 'reads' in line:
+        benchmark['metric'] = 'Read throughput, ops/sec'
+    else:
+        return
+    thr = line.split()[2].strip('(')
+    benchmark['value'] = int(float(thr))
+    post()
 
 
 def post():
@@ -32,19 +73,20 @@ def post():
 
 def main():
     for line in fileinput.input():
-        if 'write ratio' in line:
-            set_title(line)
+        # Group attributes
+        if 'working set size' in line:
+            set_documents(line)
 
-        if 'ops/sec' in line:
-            if 'writes' in line:
-                benchmark['metric'] = 'Write throughput, ops/sec'
-            elif 'reads' in line:
-                benchmark['metric'] = 'Read throughput, ops/sec'
-            else:
-                continue
-            thr = line.strip().split()[2].strip('(')
-            benchmark['value'] = int(float(thr))
-            post()
+        if 'write ratio' in line:
+            set_workload(line)
+        elif 'compaction threshold' in line:
+            set_compaction(line)
+        elif 'block reuse threshold' in line:
+            set_cbr(line)
+
+        # Metric attributes
+        elif 'ops/sec' in line:
+            set_throughput(line)
 
 
 if __name__ == '__main__':

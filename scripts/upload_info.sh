bc17a3f72d369daf0f48129820df3d0b5a8c136b
Pavel Paulau <pavel.paulau@gmail.com>
2016-08-23 15:49:47 -0700

    CBPS-158: Add script for uploading log files to S3
    Change-Id: I5b87f2e2b8274a67637280903c45c4ef7baa3ea0
    Reviewed-on: http://review.couchbase.org/67003
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

---
+++ scripts/upload_info.sh
@@ -0,0 +1,6 @@
+#!/bin/bash +x
+
+for FILE in *.zip; do
+    ./env/bin/aws s3 cp --quiet ${FILE} s3://perf-artifacts/${BUILD_TAG}/${FILE}
+    echo "https://s3-us-west-2.amazonaws.com/perf-artifacts/${BUILD_TAG}/${FILE}"
+done

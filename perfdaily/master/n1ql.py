7ced808d0353d15fa4b254acc837f395b5ec8da8
agyryk <oleksandr.gyryk@couchbase.com>
2016-08-29 14:19:12 -0700

    Daily Latency test codefix
    Change-Id: Ib4d3c50b5c4839108ad1f70e950e5f4639f6d720
    Reviewed-on: http://review.couchbase.org/67136
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Alex Gyryk <oleksandr.gyryk@couchbase.com>

--- perfdaily/master/n1ql.py
+++ perfdaily/master/n1ql.py
@@ -32,27 +32,22 @@ class N1QLLatencyTestDaily(N1QLLatencyTest):
     def run(self):
         load_settings = self.test_config.load_settings
         load_settings.items /= 2
-
         iterator = TargetIterator(self.cluster_spec, self.test_config, 'n1ql')
-        self.load(load_settings, iterator)
 
+        self.load(load_settings, iterator)
         self.load(load_settings)
+
         self.wait_for_persistence()
         self.compact_bucket()
-
         self.build_index()
-        if self.test_config.access_settings.n1ql_op != "ryow":
-            self._create_prepared_statements()
 
+        self._create_prepared_statements()
         self.workload = self.test_config.access_settings
         self.workload.items /= 2
-        self.workload.n1ql_queries = getattr(self, 'n1ql_queries',
-                                             self.workload.n1ql_queries)
-
+        self.workload.n1ql_queries = getattr(self, 'n1ql_queries',self.workload.n1ql_queries)
         self.access_bg(self.workload)
         self.access(self.workload)
 
-        if self.test_config.stats_settings.enabled:
-            self.reporter.post_to_sf(
-                *self.metric_helper.calc_avg_n1ql_queries()
-            )
+        metrics = list()
+        metrics.append(self.metric_helper.calc_query_latency_for_perfdaily(percentile=80))
+        self.reporter.post_to_dailyp(metrics)

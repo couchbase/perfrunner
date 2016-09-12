449ab381f3672530ba3735bd9b550bfe7c6cf442
Pavel Paulau <pavel.paulau@gmail.com>
2016-09-12 15:00:40 -0700

    CBFS no longer exists
    Change-Id: I0c2c65d912c64ffd3a01cea0d2277c96ea2aec0b
    Reviewed-on: http://review.couchbase.org/67601
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfrunner/utils/install.py
+++ perfrunner/utils/install.py
@@ -17,7 +17,6 @@ Build = namedtuple(
 
 class CouchbaseInstaller(object):
 
-    CBFS = 'http://cbfs-ext.hq.couchbase.com/builds/'
     LATEST_BUILDS = 'http://latestbuilds.hq.couchbase.com/'
     SHERLOCK_BUILDS = ''
     WATSON_BUILDS = ''
@@ -75,7 +74,7 @@ class CouchbaseInstaller(object):
 
     def find_package(self):
         for filename in self.get_expected_filenames():
-            for base in (self.LATEST_BUILDS, self.SHERLOCK_BUILDS, self.WATSON_BUILDS, self.SPOCK_BUILDS, self.CBFS):
+            for base in (self.LATEST_BUILDS, self.SHERLOCK_BUILDS, self.WATSON_BUILDS, self.SPOCK_BUILDS):
                 url = '{}{}'.format(base, filename)
                 try:
                     status_code = requests.head(url).status_code

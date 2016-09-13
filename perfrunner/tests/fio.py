c0627c6d8d11a9b9597b8fecd10b562d46a71521
Pavel Paulau <pavel.paulau@gmail.com>
2016-09-13 12:16:18 -0700

    CBPS-170: Send fio results to fio.sc.couchbase.com
    Change-Id: I3d7c1e3448afab86bf3a91fbbc7dc54126a1cc2f
    Reviewed-on: http://review.couchbase.org/67646
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- perfrunner/tests/fio.py
+++ perfrunner/tests/fio.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+import requests
 from logger import logger
 
 from perfrunner.helpers.misc import pretty_dict
@@ -8,6 +9,14 @@ from perfrunner.tests import PerfTest
 
 
 class FIOTest(PerfTest):
+
+    TRACKER = 'fio.sc.couchbase.com'
+
+    TEMPLATE = {
+        'group': '{}, random mixed reads and writes, IOPS',
+        'metric': None,
+        'value': None,
+    }
 
     def __init__(self, cluster_spec, test_config, verbose):
         self.cluster_spec = cluster_spec
@@ -27,6 +36,21 @@ class FIOTest(PerfTest):
                 stats[host] += int(job.split(';')[48])  # writes
         return stats
 
+    def _post(self, data):
+        data = pretty_dict(data)
+        logger.info('Posting: {}'.format(data))
+        requests.post('http://{}/api/v1/benchmarks'.format(self.TRACKER),
+                      data=data)
+
+    def _report_kpi(self, stats):
+        for host, iops in stats.items():
+            data = self.TEMPLATE.copy()
+            data['group'] = data['group'].format(self.cluster_spec.name.title())
+            data['metric'] = host
+            data['value'] = iops
+
+            self._post(data)
+
     def run(self):
         stats = self.remote.fio(self.test_config.fio['config'])
-        logger.info('IOPS: {}'.format(pretty_dict(self._parse(stats))))
+        self._report_kpi(self._parse(stats))

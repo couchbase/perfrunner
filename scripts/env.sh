428902b2e8a7fd42936d220d54c846403e3b7afa
Pavel Paulau <pavel.paulau@gmail.com>
2016-09-15 22:16:50 -0700

    Remove unused files in the "scripts" directory
    Change-Id: I5604442771200b0420d7922ac0040b06324d3536
    Reviewed-on: http://review.couchbase.org/67732
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-if [ -z "$ENV_FOLDER" ];
-then
-	ENV_FOLDER="/tmp"
-fi
-
-virtualenv -p python2.7 $ENV_FOLDER/env
-PATH=/usr/lib/ccache:/usr/lib64/ccache/bin:$PATH $ENV_FOLDER/env/bin/pip install --upgrade -r requirements.txt

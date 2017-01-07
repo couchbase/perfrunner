08e2f9e8dc6e1f4ed29f3831b85c4dd864d125db
Pavel Paulau <pavel.paulau@gmail.com>
2017-01-06 17:01:38 -0800

    CBPS-257: Add DCP tests to perfdaily
    Change-Id: Ic804d490777e139f97f7ab6f53dc830872647e6c
    Reviewed-on: http://review.couchbase.org/71714
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfdaily/kv.py
+++ perfdaily/kv.py
@@ -1,5 +1,14 @@
 from perfdaily import DailyTest
+from perfrunner.tests.dcp import DCPThroughputTest as _DCPThroughputTest
 from perfrunner.tests.kv import PillowFightTest as _PillowFightTest
+
+
+class DCPThroughputTest(DailyTest, _DCPThroughputTest):
+
+    def _report_kpi(self, throughput):
+
+        self.reporter.post_to_daily(metric='Avg Throughput (items/sec)',
+                                    value=throughput)
 
 
 class PillowFightTest(DailyTest, _PillowFightTest):

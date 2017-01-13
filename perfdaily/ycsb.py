14a16994ca3d8c55c28a03ca82b4b62f8c01d860
Pavel Paulau <pavel.paulau@gmail.com>
2017-01-13 14:08:20 -0800

    CBPS-268: Add YCSB workload A and C benchmarks to perfdaily
    Change-Id: Ib1b1fab16923dbcec3b694b974268bccc2f4b61b
    Reviewed-on: http://review.couchbase.org/71998
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfdaily/ycsb.py
+++ perfdaily/ycsb.py
@@ -1,5 +1,21 @@
 from perfdaily import DailyTest
 from perfrunner.tests.ycsb2 import YCSBN1QLTest as _YCSBN1QLTest
+from perfrunner.tests.ycsb2 import YCSBTest as _YCSBTest
+
+
+class YCSBTest(DailyTest, _YCSBTest):
+
+    def run(self):
+        self.remote.disable_cpu()
+
+        super(YCSBTest, self).run()
+
+        self.remote.enable_cpu()
+
+    def _report_kpi(self):
+        self.reporter.post_to_daily(
+            *self.metric_helper.cal_ycsb_throughput()
+        )
 
 
 class YCSBN1QLTest(DailyTest, _YCSBN1QLTest):

c616d6dc51e4f90e4accd880a6bda8f5677a4651
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-03-01 10:08:20 -0800

    FTS: temporary revert the RBAC authorization
    Change-Id: I6fee4eedd85f49fb8af1a194fe78a17f58e7ae1a
    Reviewed-on: http://review.couchbase.org/74476
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Alex Gyryk <oleksandr.gyryk@couchbase.com>

--- spring/wgen.py
+++ spring/wgen.py
@@ -736,13 +736,12 @@ class WorkloadGen(object):
         lock = Lock()
         worker_type, total_workers = worker_factory(self.ws)
         if name == 'fts' and total_workers:
-
             master_host = self.ts.node.split(":")[0]
             if self.ws.fts_config.elastic:
                 rest_auth = (self.ws.fts_config.username, self.ts.password)
                 self.ws.fts_query_list = ElasticGen(master_host, self.ws.fts_config, rest_auth).query_list
             else:
-                rest_auth = (self.ts.bucket, self.ts.password)
+                rest_auth = (self.ws.fts_config.username, self.ts.password)
                 self.ws.fts_query_list = FtsGen(master_host, self.ws.fts_config, rest_auth).query_list
             self.ws.fts_query_list_size = len(self.ws.fts_query_list)
 

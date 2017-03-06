33a87081422a52f57785ebca683992b5bd362c7a
Pavel Paulau <pavel.paulau@gmail.com>
2017-03-06 13:43:26 -0800

    Use a better error message when clusters are not balanced
    Change-Id: I058f6cfb78b68ec64e3448dce6169b48347a0d1a
    Reviewed-on: http://review.couchbase.org/74728
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfrunner/tests/__init__.py
+++ perfrunner/tests/__init__.py
@@ -79,7 +79,7 @@ class PerfTest(object):
 
             for master in self.cluster_spec.yield_masters():
                 if not self.rest.is_balanced(master):
-                    logger.interrupt('Rebalance failed')
+                    logger.interrupt('The cluster is not balanced')
 
                 self.check_failover(master)
 

7dbe595245fb14e4d10cb9b2dbb1a9f4c1e76d43
Pavel Paulau <pavel.paulau@gmail.com>
2017-04-27 12:06:11 -0700

    Report the progress when compacting Seriesly databases
    Change-Id: I604f0eb871a7854c87282f19a3c6404d907d38ac
    Reviewed-on: http://review.couchbase.org/77459
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/compact_seriesly.py
+++ scripts/compact_seriesly.py
@@ -6,8 +6,10 @@ from perfrunner.settings import StatsSettings
 
 def main():
     s = Seriesly(StatsSettings.SERIESLY)
-    for db in s.list_dbs():
-        logger.info('Compacting {}'.format(db))
+
+    dbs = s.list_dbs()
+    for i, db in enumerate(dbs, start=1):
+        logger.info('Compacting {} ({} / {})'.format(db, i, len(dbs)))
         result = s[db].compact()
         logger.info('Compaction finished: {}'.format(result))
 

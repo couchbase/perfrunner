865ddb4050f676647976f110b33f656f0d00b3f9
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-07-13 13:39:51 -0700

    FTS DGM - test configs, data and utils
    Change-Id: I3487a3ed884a2b803f708c47ff87d97543a8df59
    Reviewed-on: http://review.couchbase.org/80676
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Alex Gyryk <oleksandr.gyryk@couchbase.com>

---
+++ perfrunner/utils/fts/dgm/create_dataset_dgm.py
@@ -0,0 +1,211 @@
+import random
+
+from couchbase.bucket import Bucket
+
+
+class NumericExctractor:
+    def __init__(self, cb_url, bucket_name, items):
+        self.distance = 39000
+        self.output_lines_goal = 1000
+        self.total_docs = items
+        self.cb = Bucket("couchbase://{}/{}?operation_timeout=10".format(cb_url, bucket_name), password="password")
+        self.output_list = list()
+
+    def run(self):
+        n = 0
+        i = 0
+        while i<self.output_lines_goal:
+            n, min = self.read_number(n)
+            max = min + random.randint(999999, 9999999999)
+            self.output_list.append("{}   max".format(max))
+            self.output_list.append("{}   min".format(min))
+            self.output_list.append("{}:{}   max_min".format(max, min))
+            i += 1
+            print(i)
+        self.write_and_exit()
+
+    def read_number(self, n):
+        step = random.randint(0, self.distance)
+        n = n + step
+        if n >= self.total_docs:
+            self.write_and_exit()
+        k = hex(n)[2:]
+        v = self.cb.get(k).value
+        return n, int(v['time'])
+
+    def write_and_exit(self):
+        f = open("numeric-dgm.txt", "w")
+        f.write('\n'.join(self.output_list))
+        f.close()
+
+
+class TermProcessor:
+    def __init__(self):
+        self.limit = 2000
+
+    def _shuffle_and_cut(self, fpath, limit=500):
+        f = open(fpath)
+        l = list(f)
+        random.shuffle(l)
+        l = l[:limit]
+        f.close()
+        return l
+
+    def combine_2(self, output_name, file1, file2):
+        lines1 = self._shuffle_and_cut(file1)
+        lines2 = self._shuffle_and_cut(file2)
+
+        map = set()
+        for line1 in lines1:
+            line1 = line1.split()[0]
+            for line2 in lines2:
+                line2 = line2.split()[0]
+                if line1 != line2:
+                    direct = "{} {}".format(line1, line2)
+                    reverse = "{} {}".format(line2, line1)
+                    if reverse not in map:
+                        map.add(direct)
+        results = list(map)
+        random.shuffle(results)
+        results = results[:self.limit]
+
+        result_file = open(output_name, "w")
+        for line in results:
+            result_file.write("{}\n".format(line))
+        result_file.close()
+
+    def combine_3(self, output_name, file1, file2, file3):
+        lines1 = self._shuffle_and_cut(file1)
+        lines2 = self._shuffle_and_cut(file2)
+        lines3 = self._shuffle_and_cut(file3)
+        map = set()
+        for line1 in lines1:
+            line1 = line1.split()[0]
+            for line2 in lines2:
+                line2 = line2.split()[0]
+                for line3 in lines3:
+                    line3 = line3.split()[0]
+                    if line2 != line3:
+                        direct = "{} {} {}".format(line1, line2, line3)
+                        reverse = "{} {} {}".format(line1, line3, line2)
+                        if reverse not in map:
+                            map.add(direct)
+        results = list(map)
+        random.shuffle(results)
+        results = results[:self.limit]
+        result_file = open(output_name, "w")
+        for line in results:
+            result_file.write("{}\n".format(line))
+        result_file.close()
+
+    def get_fuzzies(self, output1_file, output2_file, input_file, size=5):
+        lines = self._shuffle_and_cut(input_file, 10 ** 6)
+        sized_lines = list()
+        items = 0
+        for line in lines:
+            line = line.split()[0]
+            if len(line) == size:
+                sized_lines.append(line)
+                items = len(sized_lines)
+            if items == self.limit:
+                break
+
+        output1_file = open(output1_file, "w")
+        output2_file = open(output2_file, "w")
+        for line in sized_lines:
+            output1_file.write("{} {}\n".format(line, "1"))
+            output2_file.write("{} {}\n".format(line, "2"))
+        output1_file.close()
+        output2_file.close()
+
+    def get_wildcard(self, output_file, input_file):
+        lines = self._shuffle_and_cut(input_file, self.limit)
+        of = open(output_file, "w")
+        for line in lines:
+            line = line.split()[0]
+            if len(line) == 4:
+                term = line
+                term = line[:2] + '*' + line[3:]
+                of.write("{}\n".format(term))
+        of.close()
+
+    def get_prefix(self, output_file, input_file):
+        lines = self._shuffle_and_cut(input_file, 10 ** 6)
+        map = set()
+        for line in lines:
+            line = line.split()[0]
+            if len(line) > 3:
+                map.add(line[:3])
+        map = list(map)[:self.limit]
+        output_file = open(output_file, "w")
+        for line in map:
+            output_file.write("{}\n".format(line))
+        output_file.close()
+
+    def get_phrases(self, cb_url, output_file, input_file, docs_total):
+        cb = Bucket("couchbase://{}/{}?operation_timeout=10".format(cb_url, "bucket-1"), password="password")
+        lines = self._shuffle_and_cut(input_file, 10 ** 6)
+        formatted_lines = list()
+        for line in lines:
+            formatted_lines.append(line.split()[0])
+        lines = formatted_lines
+        results = set()
+        for docid in range(1, docs_total - 1):
+            key = hex(random.randint(1, docs_total))[2:]
+            try:
+                txt = cb.get(key).value
+                if txt["text"]:
+                    txt = txt["text"].encode('ascii', 'ignore')
+                    terms = txt.split(' ')
+                    for idx, term in enumerate(terms):
+                        if term in lines:
+                            if len(terms) > idx + 1:
+                                term_next = terms[idx + 1]
+                                if str.isalpha(term_next):
+                                    result_phrase = "{} {}".format(term, term_next)
+                                    results.add(result_phrase)
+            except Exception as e:
+                print(("{}: {}: {}".format(key, len(results), str(e))))
+
+            if len(results) > self.limit:
+                break
+
+        output_file = open(output_file, "w")
+        for phrase in results:
+            output_file.write("{}\n".format(phrase))
+
+    def get_dates(self, output_file, input_file):
+        lines = self._shuffle_and_cut(input_file, self.limit)
+        output_file = open(output_file, "w")
+        for line in lines:
+            line = line.split()[0]
+
+            y = random.randint(2007, 2017)
+            m1 = random.randint(1, 12)
+            m2 = random.randint(1, 12)
+            if m1 > m2:
+                b = m1
+                m1 = m2
+                m2 = b
+            d1 = random.randint(1, 30)
+            d2 = random.randint(1, 30)
+            date1 = "{}-{}-{}".format(y, m1, d1)
+            date2 = "{}-{}-{}".format(y, m2, d2)
+
+            result = "{} {}:{}".format(line, date1, date2)
+            output_file.write("{}\n".format(result))
+
+'''
+nex = NumericExctractor("172.23.99.211", "bucket-1", 20000000)
+nex.run()
+
+'''
+termpro = TermProcessor()
+#termpro.combine_2("hi_hi.txt", "hi.txt", "hi.txt")
+#termpro.combine_2("hi_med.txt", "hi.txt", "med.txt")
+#termpro.combine_3("hi_med_med.txt", "hi.txt", "med.txt", "med.txt")
+#termpro.combine_3("med_hi_hi.txt", "med.txt", "hi.txt", "hi.txt")
+termpro.get_fuzzies("fuzzy_1.txt", "fuzzy_2.txt", "med.txt")
+termpro.get_wildcard("wildcard.txt", "med.txt")
+#termpro.get_phrases("172.23.99.211", "phrase.txt", "med.txt", 1000000)
+#termpro.get_dates("date.txt", "hi.txt")
---
+++ perfrunner/utils/fts/dgm/load_data_dgm.py
@@ -0,0 +1,141 @@
+
+import datetime
+import itertools
+
+import numpy
+from couchbase.bucket import Bucket
+
+
+class GenericDocgen:
+    def __init__(self, master_file_path, cb_url, bucket_name, chunk_size=1024):
+        self.chunk_size = chunk_size
+        self.items = 100
+        self.master_file_path = master_file_path
+        self.dateBuilder = DateBuilder()
+        self.numBuilder = NumericBuilder(self.items)
+
+        self.c = Bucket("couchbase://{}/{}?operation_timeout=10".format(cb_url, bucket_name), password="password")
+
+    def read_file_gen(self, file):
+        while True:
+            data = file.read(self.chunk_size)
+            if not data:
+                break
+            yield data
+
+    def build(self, master_file):
+        self.master_lines = self.read_file_gen(master_file)
+        doc = {}
+        doc["text"] = self.master_lines.next()
+        doc["text2"] = self.master_lines.next()
+        doc["time"] = self.numBuilder.build()
+        doc["date"] = self.dateBuilder.build()
+        return doc
+
+    def run(self):
+        master_file = open(self.master_file_path)
+        i = 0
+        while i < self.items:
+            key = hex(i)[2:]
+            val = self.build(master_file)
+            self.c.upsert(key, val)
+            try:
+                saved = self.c.get(key).value
+                if 'text' in saved:
+                    i += 1
+            except Exception as e:
+                pass
+        master_file.close()
+
+
+class NumericBuilder:
+    def __init__(self, items):
+        self.items = items
+        self.timenow = 0
+        self.month = itertools.cycle(range(1, 12))
+        self.days = itertools.cycle(range(1, 28))
+        self.hours = itertools.cycle(range(24))
+        self.minutes = itertools.cycle(range(60))
+        self.seconds = itertools.cycle(range(60))
+        self.milis = itertools.cycle(range(48000, 483163))
+        self.years_list = list(range(2007, 2017))
+        self.years_weighted = [self.years_list[9]] * int(0.3  * items) + \
+                              [self.years_list[8]] * int(0.17 * items) + \
+                              [self.years_list[7]] * int(0.12 * items) + \
+                              [self.years_list[6]] * int(0.09 * items) + \
+                              [self.years_list[5]] * int(0.08 * items) + \
+                              [self.years_list[4]] * int(0.06 * items) + \
+                              [self.years_list[3]] * int(0.05 * items) + \
+                              [self.years_list[2]] * int(0.05 * items) + \
+                              [self.years_list[1]] * int(0.04 * items) + \
+                              [self.years_list[0]] * int(0.04 * items)
+        self.years_weighted.reverse()
+        self.years = itertools.cycle(self.years_weighted)
+
+    def build(self):
+        return self.time_milis()
+
+    def form_date(self):
+        return (
+            next(self.years),
+            next(self.month),
+            next(self.days),
+            next(self.hours),
+            next(self.minutes),
+            next(self.seconds),
+            next(self.milis)
+        )
+
+    def time_milis(self):
+        self.timenow = datetime.datetime(*self.form_date())
+        return int(self.timenow.strftime("%s")) * 1000
+
+
+
+class DateBuilder:
+    def __init__(self):
+        yrange = range(2007, 2017)
+        yrange.reverse()
+
+        mrange = range(1, 12)
+        mrange.reverse()
+
+        drange = range(1, 30)
+        drange.reverse()
+
+        self.years = itertools.cycle(yrange)
+        self.month = itertools.cycle(mrange)
+        self.days = itertools.cycle(drange)
+        self.dates_list = list()
+        self.total_size = 10*12*30
+        self.form_dates_list()
+
+    def form_dates_list(self):
+        for id in range(self.total_size):
+            self.dates_list.append('{}-{}-{}'.format(next(self.years), next(self.month), next(self.days)))
+
+    def build(self):
+        index = self.zipf()
+        while index[0] >= len(self.dates_list):
+            index = self.zipf()
+        return self.dates_list[index[0]]
+
+    def zipf(self):
+        return numpy.random.zipf([1.2])
+
+
+docgen = GenericDocgen('output.txt', '172.23.99.211', 'bucket-1')
+docgen.run()
+
+'''
+Usage templates:
+A = Numeric('xaa','xbb', '172.23.99.211', 'bucket-1')
+A.start_load()
+A.create_document()
+
+A = Docgen('xaa','xab','172.23.99.39','bucket-1')
+A.start_load()
+
+A = Datefacet()
+A.run()
+'''

2f72f75da7ba03e331927c5ab0a5702c150b2f9a
Pavel Paulau <pavel.paulau@gmail.com>
2017-07-18 11:45:15 -0700

    Use broker IP instead of domain name
    Change-Id: Ide27c97a00c18ac62c1a92e2ec51c74c5af4cf30
    Reviewed-on: http://review.couchbase.org/81029
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfrunner/celeryremote.py
+++ perfrunner/celeryremote.py
@@ -1,4 +1,4 @@
-BROKER_URL = 'amqp://couchbase:couchbase@ci.sc.couchbase.com:5672/broker'
+BROKER_URL = 'amqp://couchbase:couchbase@172.23.97.73:5672/broker'
 CELERY_RESULT_BACKEND = 'amqp'
 CELERY_RESULT_EXCHANGE = 'perf_results'
 CELERY_RESULT_PERSISTENT = False

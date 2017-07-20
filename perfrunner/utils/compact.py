9ec2fa787c10ab3960c0883ffb92ed5a2519b1cc
Pavel Paulau <pavel.paulau@gmail.com>
2017-07-20 15:05:30 -0700

    Shuffle the list of databases before compaction
    Change-Id: Iedf34388ca3f7199ee22a4285909b82f46a139f8
    Reviewed-on: http://review.couchbase.org/81209
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfrunner/utils/compact.py
+++ perfrunner/utils/compact.py
@@ -1,3 +1,5 @@
+import random
+
 from seriesly import Seriesly
 
 from logger import logger
@@ -8,7 +10,8 @@ def main():
     s = Seriesly(StatsSettings.SERIESLY)
 
     dbs = s.list_dbs()
-    for i, db in enumerate(reversed(dbs), start=1):
+    random.shuffle(dbs)
+    for i, db in enumerate(dbs, start=1):
         logger.info('Compacting {} ({} / {})'.format(db, i, len(dbs)))
         result = s[db].compact()
         logger.info('Compaction finished: {}'.format(result))

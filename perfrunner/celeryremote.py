1227461e674588459066c3605e5c8bf0ecbd39d0
Pavel Paulau <pavel.paulau@gmail.com>
2017-08-21 12:17:46 -0700

    Establish and close connections for every use
    Celery maintains a pool of connections (10 by default) that can be
    reused in different test phases.
    
    In our case it seems safer to establish a new connection explicitly
    instead of reusing the existing one.
    
    See also:
    
    http://docs.celeryproject.org/en/3.1/configuration.html#broker-pool-limit
    
    Change-Id: I97c67a73041ca5a84c467380eda420fcbb980609
    Reviewed-on: http://review.couchbase.org/82516
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfrunner/celeryremote.py
+++ perfrunner/celeryremote.py
@@ -1,4 +1,5 @@
 BROKER_URL = 'amqp://couchbase:couchbase@172.23.97.73:5672/broker'
+BROKER_POOL_LIMIT = None
 CELERY_RESULT_BACKEND = 'amqp'
 CELERY_RESULT_EXCHANGE = 'perf_results'
 CELERY_RESULT_PERSISTENT = False

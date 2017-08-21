e8d57ef08616b06e5f94da7e01ba96c13b9124d7
Pavel Paulau <pavel.paulau@gmail.com>
2017-08-21 16:49:08 -0700

    Disable hijacking of previously configured log handlers
    See also:
    
    http://docs.celeryproject.org/en/3.1/configuration.html#celeryd-hijack-root-logger
    
    Change-Id: Ibf4618e8bfeb28f877db4a40b4a911ff00442cc9
    Reviewed-on: http://review.couchbase.org/82543
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfrunner/celeryremote.py
+++ perfrunner/celeryremote.py
@@ -2,3 +2,4 @@ BROKER_URL = 'amqp://couchbase:couchbase@172.23.97.73:5672/broker'
 CELERY_RESULT_BACKEND = 'amqp'
 CELERY_RESULT_EXCHANGE = 'perf_results'
 CELERY_RESULT_PERSISTENT = False
+CELERYD_HIJACK_ROOT_LOGGER = False

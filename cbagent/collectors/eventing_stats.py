2c39a7ff0dff0bfa5bf07442bf59a83d9fd40e33
Mahesh Mandhare <mahesh.mandhare@couchbase.com>
2017-10-31 11:54:46 +0530

    Eventing: Adding failure stats
    Change-Id: I187c43011373f4c1cbfbb88ab13ce77d22eb753a
    Reviewed-on: http://review.couchbase.org/84920
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- cbagent/collectors/eventing_stats.py
+++ cbagent/collectors/eventing_stats.py
@@ -4,21 +4,28 @@ from cbagent.collectors import Collector
 class EventingStats(Collector):
 
     COLLECTOR = "eventing_stats"
+    EVENTING_PORT = 8096
 
     def __init__(self, settings, test):
         super().__init__(settings)
         self.eventing_node = test.function_nodes[0]
         self.functions = test.functions
 
-    def _get_processing_stats(self, function_name="perf-test1"):
-        port = '8096'
+    def _get_processing_stats(self, function_name):
         uri = "/getEventProcessingStats?name={}".format(function_name)
-        samples = self.get_http(path=uri, server=self.eventing_node, port=port)
+        samples = self.get_http(path=uri, server=self.eventing_node, port=self.EVENTING_PORT)
+        return samples
+
+    def _get_failure_stats(self, function_name):
+        uri = "/getFailureStats?name={}".format(function_name)
+        samples = self.get_http(path=uri, server=self.eventing_node, port=self.EVENTING_PORT)
         return samples
 
     def sample(self):
         for name, function in self.functions.items():
             stats = self._get_processing_stats(function_name=name)
+            failure_stats = self._get_failure_stats(function_name=name)
+            stats.update(failure_stats)
             if stats:
                 self.update_metric_metadata(stats.keys(), bucket=name)
                 self.store.append(stats, cluster=self.cluster,

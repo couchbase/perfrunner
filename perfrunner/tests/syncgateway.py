cce78141615a1fc331c4e1350343cf0fc5c0b4e0
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-11-02 13:19:05 -0700

    sunc

--- perfrunner/tests/syncgateway.py
+++ perfrunner/tests/syncgateway.py
@@ -79,7 +79,7 @@ class SGPerfTest(PerfTest):
             if not os.path.exists("YCSB"):
                 os.makedirs("YCSB")
             self.remote.get_syncgateway_YCSB_logs(self.worker_manager.WORKER_HOME,
-                                                  int(self.test_config.syncgateway_settings))
+                                                  self.test_config.syncgateway_settings)
 
     def run_sg_phase(self,
                   phase: str,

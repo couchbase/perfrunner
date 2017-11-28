1b56c7783746d760668b3cbc0cf5ea0d71185896
Mahesh Mandhare <mahesh.mandhare@couchbase.com>
2017-11-28 13:45:59 +0530

    Fix eventing timer n1ql application code for spelling
    Change-Id: I578a10b18d595174ad5303ea3072767eca66d031
    Reviewed-on: http://review.couchbase.org/86052
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Mahesh Mandhare <mahesh.mandhare@couchbase.com>

--- tests/eventing/code/timer_n1ql.js
+++ tests/eventing/code/timer_n1ql.js
@@ -2,8 +2,8 @@ function OnUpdate(doc, meta) {
 	expiry = Math.round((new Date()).getTime() / 1000) + 300;
 	docTimer(timerCallback, meta.id, expiry);
 }
-function timerCallback(docid) {
-	var query = SELECT * FROM `bucket-1` USE KEYS[:docid];
+function timerCallback(docId) {
+	var query = SELECT * FROM `bucket-1` USE KEYS[:docId];
 	query.execQuery();
 	query = UPSERT INTO `eventing-bucket-1` ( KEY, VALUE ) VALUES ( :docId, 'Hello World');
 	query.execQuery();

fd1a7d47aa7fb9a18ba3b86e1d83bd5d6e273b58
Mahesh Mandhare <mahesh.mandhare@couchbase.com>
2018-02-26 13:26:58 +0530

    Eventing: metric id fix for rebalance time
    Change-Id: I59a5123b3d0f3cb6da8cc740b03bd1a97839f563
    Reviewed-on: http://review.couchbase.org/90042
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Mahesh Mandhare <mahesh.mandhare@couchbase.com>

--- perfrunner/helpers/metrics.py
+++ perfrunner/helpers/metrics.py
@@ -665,8 +665,8 @@ class MetricHelper:
     def eventing_rebalance_time(self, time: int) -> Metric:
         title_split = self._title.split(sep=",", maxsplit=2)
         title = "Rebalance Time(sec)," + title_split[1]
-        metric_info = self._metric_info(title=title)
-
+        metric_id = '{}_rebalance_time'.format(self.test_config.name)
+        metric_info = self._metric_info(metric_id=metric_id, title=title)
         return time, self._snapshots, metric_info
 
     def function_latency(self, percentile: float, latency_stats: dict) -> Metric:

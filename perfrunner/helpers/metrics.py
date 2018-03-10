b4540b6410f13b813dab26186c147f4270e03f50
Pavel Paulau <pavel.paulau@gmail.com>
2018-03-10 08:23:45 -0800

    Sort Analytics results by storage type
    Change-Id: I2d3a5c23fe656eb66f237e6099e005a93912dfce
    Reviewed-on: http://review.couchbase.org/90775
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- perfrunner/helpers/metrics.py
+++ perfrunner/helpers/metrics.py
@@ -695,7 +695,7 @@ class MetricHelper:
                                                             query['description'],
                                                             self._title)
 
-        order_by = '{:05d}'.format(int(query['name'][2:]))
+        order_by = '{:05d}_{}'.format(int(query['name'][2:]), self._order_by)
 
         metric_info = self._metric_info(metric_id,
                                         title,

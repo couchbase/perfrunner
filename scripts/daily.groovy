debb7e0532aa8777be9910702b77a5d3dea65634
Pavel Paulau <pavel.paulau@gmail.com>
2018-05-09 13:37:44 -0700

    Read pipeline inputs from JSON files
    Change-Id: I7cd296d8731c8b4629e659e7363d4d3d0abf2f22
    Reviewed-on: http://review.couchbase.org/93952
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

---
+++ scripts/daily.groovy
@@ -0,0 +1,75 @@
+currentBuild.description = params.version
+
+def testCases = null
+
+def build_all(tests) {
+    for ( test in tests ) {
+        build job: 'triton', propagate: false, parameters: [
+            string(name: 'test_config', value: test['test_config']),
+            string(name: 'cluster', value: test['cluster']),
+            string(name: 'version', value: params.version)
+        ]
+    }
+}
+
+pipeline {
+    agent {label 'master'}
+    stages {
+        stage('Setup') {
+            steps {
+                script {
+                    testCases = readJSON file: params.test_suite
+                }
+            }
+        }
+        stage('Eventing') {
+            steps {
+                build_all(testCases['Eventing'])
+            }
+        }
+        stage('FTS') {
+            steps {
+                build_all(testCases['FTS'])
+            }
+        }
+        stage('GSI') {
+            steps {
+                build_all(testCases['GSI'])
+            }
+        }
+        stage('KV') {
+            steps {
+                build_all(testCases['KV'])
+            }
+        }
+        stage('N1QL') {
+            steps {
+                build_all(testCases['N1QL'])
+            }
+        }
+
+        stage('Tools') {
+            steps {
+                build_all(testCases['Tools'])
+            }
+        }
+
+        stage('XDCR') {
+            steps {
+                build_all(testCases['XDCR'])
+            }
+        }
+
+        stage('YCSB') {
+            steps {
+                build_all(testCases['YCSB'])
+            }
+        }
+
+        stage('Notifications') {
+            steps {
+                build job: 'triton-notification', parameters: [string(name: 'version', value: params.version)]
+            }
+        }
+    }
+}

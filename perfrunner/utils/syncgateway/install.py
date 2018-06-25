808e5f6a5584c75bfb02ecf68ff39e47a4229bfd
sharath chandra <sharujayaram@gmail.com>
2018-06-25 12:14:30 -0700

    base_url changed for syncgateway 2.0.0

--- perfrunner/utils/syncgateway/install.py
+++ perfrunner/utils/syncgateway/install.py
@@ -3,6 +3,7 @@ import os
 from perfrunner.utils.syncgateway.ansible_runner import AnsibleRunner
 from logger import logger
 
+
 def main():
 
     _uninstall = False
@@ -18,7 +19,10 @@ def main():
 
     v, b = _build.split("-")
 
-    base_url = "http://latestbuilds.service.couchbase.com/builds/latestbuilds/sync_gateway/{}/{}".format(v, b)
+    if v == "2.0.0":
+        base_url = "http://latestbuilds.service.couchbase.com/builds/releases/mobile/couchbase-sync-gateway/2.0.0"
+    else:
+        base_url = "http://latestbuilds.service.couchbase.com/builds/latestbuilds/sync_gateway/{}/{}".format(v, b)
     sg_package_name = "couchbase-sync-gateway-enterprise_{}_x86_64.rpm".format(_build)
     accel_package_name = "couchbase-sg-accel-enterprise_{}_x86_64.rpm".format(_build)
 
@@ -34,18 +38,16 @@ def main():
         playbook = "remove-previous-installs.yml"
     else:
         playbook = "install-sync-gateway-package.yml"
-
     ansible_runner = AnsibleRunner(_cluster_path)
-
-
 
     status = ansible_runner.run_ansible_playbook(playbook, extra_vars=playbook_vars)
     if status != 0:
         logger.info("Failed to install sync_gateway package")
         sys.exit(1)
 
+
 if __name__ == '__main__':
     main()
 
 
-#env/bin/sg_install -build 1.5.0-582 -cluster perfrunner/utils/syncgateway/clusters/hebe_4node.cluster -config perfrunner/utils/syncgateway/configs/hebe_xattr_sync.config
+# env/bin/sg_install -build 1.5.0-582 -cluster perfrunner/utils/syncgateway/clusters/hebe_4node.cluster -config perfrunner/utils/syncgateway/configs/hebe_xattr_sync.config

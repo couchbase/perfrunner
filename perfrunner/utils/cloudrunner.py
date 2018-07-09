f8e4b1e0df2eaa66577e802f840bd8ad90878d8c
sharath chandra <sharujayaram@gmail.com>
2018-07-09 12:03:40 -0700

    adding new server instance type for aws testing
    Change-Id: Ic9ed4ede4a426c065c08b43d6a648f62e8c67287
    Reviewed-on: http://review.couchbase.org/96702
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <sharujayaram@gmail.com>

--- perfrunner/utils/cloudrunner.py
+++ perfrunner/utils/cloudrunner.py
@@ -130,7 +130,8 @@ def get_args():
                                  'm4.16xlarge',
                                  'r4.2xlarge',
                                  'r4.4xlarge',
-                                 'r4.8xlarge'])
+                                 'r4.8xlarge',
+                                 'i3.8xlarge'])
     parser.add_argument('action',
                         choices=['launch', 'terminate'])
 

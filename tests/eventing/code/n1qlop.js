00b91f6e2b2351d3a5eeb07c7533cbecee5f4100
Mahesh Mandhare <mahesh.mandhare@couchbase.com>
2019-06-21 16:25:51 +0530

    Draft: New way of N1QL queries in eventing
    Change-Id: I19811104204b0d1fb5c718fe0c5facf5b938a20c
    Reviewed-on: http://review.couchbase.org/111003
    Reviewed-by: Gautham B A <gautham.bangalore@gmail.com>
    Tested-by: Build Bot <build@couchbase.com>

--- tests/eventing/code/n1qlop.js
+++ tests/eventing/code/n1qlop.js
@@ -1,9 +1,12 @@
 function OnUpdate(doc, meta) {
 	var docId = meta.id;
-	var query = SELECT * FROM `bucket-1` USE KEYS[$docId];
-	query.execQuery();
-	UPSERT INTO `eventing-bucket-1` (KEY, VALUE) VALUES ($docId, 'Hello World');
+	var select_query = SELECT * FROM `bucket-1` USE KEYS[$docId];
+	for (var r of select_query) {
+	}
+	select_query.close()
+	var upsert_query = UPSERT INTO `eventing-bucket-1` (KEY, VALUE) VALUES ($docId, 'Hello World');
+	upsert_query.close();
 }
 
-function OnDelete(doc) {
+function OnDelete(meta) {
 }

79fb7274e6d4524cdaf2de6ab1d9fa8514418235
Bo-Chun Wang <bo-chun.wang@couchbase.com>
2019-11-22 23:26:47 -0800

    update release number and add daily test set for cheshire-cat
    Change-Id: I1a564e632499deb6d8527bfa02754f84af196b2d
    Reviewed-on: http://review.couchbase.org/118401
    Reviewed-by: <sharujayaram@gmail.com>
    Tested-by: <sharujayaram@gmail.com>

--- perfrunner/utils/trigger.py
+++ perfrunner/utils/trigger.py
@@ -14,9 +14,10 @@ MAX_MISSING = 3
 
 RELEASES = {
     'spock': '5.0.0',
-    'vulcan': '5.5.0',
-    'alice': '6.0.0',
-    'mad-hatter': '6.5.0'
+    'vulcan': '5.5.6',
+    'alice': '6.0.4',
+    'mad-hatter': '6.5.0',
+    'cheshire-cat': '7.0.0'
 }
 
 

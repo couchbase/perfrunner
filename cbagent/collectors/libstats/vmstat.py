cfe3a748b1ea582924c33045bd238af2d949ee60
Bo-Chun Wang <bo-chun.wang@couchbase.com>
2020-07-15 10:00:39 -0700

    update vmstat to add allocstall numbers up
    Change-Id: I93309918a73c0166f6d4a2c02f21443420ad9c24
    Reviewed-on: http://review.couchbase.org/c/perfrunner/+/132601
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- cbagent/collectors/libstats/vmstat.py
+++ cbagent/collectors/libstats/vmstat.py
@@ -4,12 +4,15 @@ from cbagent.collectors.libstats.remotestats import RemoteStats, parallel_task
 class VMStat(RemoteStats):
 
     def get_vmstat(self) -> dict:
-        stats = {}
+        stats = {'allocstall': 0}
         stdout = self.run('cat /proc/vmstat')
         for line in stdout.splitlines():
             fields = line.split()
             metric, value = fields[0], fields[1]
-            stats[metric] = int(value)
+            if 'allocstall' in metric:
+                stats['allocstall'] += int(value)
+            else:
+                stats[metric] = int(value)
         return stats
 
     @parallel_task(server_side=True)

e0d782061f7301a41007d1597e02c795b590fb6b
Prajwal <prajwal.kirankumar@couchbase.com>
2020-08-20 12:52:51 +0530

    Celery changes to increase timeout and retries
    Change-Id: I61dd18a03cefb372991a02426cfd7bd73d4a585d
    Reviewed-on: http://review.couchbase.org/c/perfrunner/+/134704
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: <sharujayaram@gmail.com>

--- perfrunner/celeryremote.py
+++ perfrunner/celeryremote.py
@@ -1,9 +1,12 @@
 broker_url = 'amqp://couchbase:couchbase@172.23.97.73:5672/broker'
 broker_pool_limit = None
 worker_hijack_root_logger = False
-result_backend = "amqp://"
+result_backend = "rpc://"
 result_persistent = False
 result_exchange = "perf_results"
 accept_content = ['pickle']
 result_serializer = 'pickle'
 task_serializer = 'pickle'
+broker_connection_timeout = 60
+broker_connection_retry = True
+broker_connection_max_retries = 100

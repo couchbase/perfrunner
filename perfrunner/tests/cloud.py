86b84c2a630d656f3ecbe19b30e7816c727e5932
Bo-Chun Wang <bo-chun.wang@couchbase.com>
2021-06-04 11:58:42 -0700

    add idle tests with kv/n1ql data loaded
    Change-Id: I16da39c45a7aebafd6464200a994e6cadac6909d
    Reviewed-on: http://review.couchbase.org/c/perfrunner/+/155130
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- perfrunner/tests/cloud.py
+++ perfrunner/tests/cloud.py
@@ -2,6 +2,7 @@ import time
 
 from perfrunner.helpers.cbmonitor import with_stats
 from perfrunner.tests import PerfTest
+from perfrunner.tests.ycsb import YCSBN1QLTest
 
 
 class CloudTest(PerfTest):
@@ -66,4 +67,42 @@ class CloudIdleDocsTest(CloudIdleTest):
         self.check_num_items()
         self.compact_bucket()
         self.access()
+        self.report_kpi()
+
+
+class CloudIdleKVN1QLTest(YCSBN1QLTest, CloudIdleTest):
+
+    def __init__(self, *args):
+        CloudIdleTest.__init__(self, *args)
+
+    def _report_kpi(self):
+        self.reporter.post(
+            *self.metrics.cpu_utilization()
+        )
+
+        for process in self.server_processes:
+            self.reporter.post(
+                *self.metrics.avg_server_process_cpu(process)
+            )
+
+    @with_stats
+    def access(self, *args):
+        time.sleep(self.test_config.access_settings.time)
+
+    def run(self):
+        if self.test_config.access_settings.ssl_mode == 'data':
+            self.download_certificate()
+            self.generate_keystore()
+        self.download_ycsb()
+
+        self.create_indexes()
+        self.wait_for_indexing()
+
+        self.load()
+        self.wait_for_persistence()
+        self.check_num_items()
+        self.wait_for_indexing()
+
+        self.access()
+
         self.report_kpi()

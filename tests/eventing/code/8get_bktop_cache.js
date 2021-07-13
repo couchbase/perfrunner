7c0555fa43a67ba3af1c21407ba793b099f1ed9a
vikas chaudhary <vikas.chaudhary@couchbase.com>
2021-07-13 13:11:32 +0530

    CBPS-939 : cache test improvements with 8 gets
    Change-Id: Ie7ff99cb5ea74da02b8fc1e9850dc2bda568ebcc
    Reviewed-on: http://review.couchbase.org/c/perfrunner/+/157394
    Reviewed-by: vikas chaudhary <vikas.chaudhary@couchbase.com>
    Tested-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

---
+++ tests/eventing/code/8get_bktop_cache.js
@@ -0,0 +1,9 @@
+function OnUpdate(doc, meta) {
+    for(var i =0;i<8;i++){
+        var result = couchbase.get(src,{ "id":"000000000000"},{"cache": true});
+    }
+}
+
+
+function OnDelete(doc) {
+}

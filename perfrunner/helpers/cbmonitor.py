d1eab021a1a787a407c3175cf9851b32e52e3193
Daniel Nagy <daniel.nagy@couchbase.com>
2023-02-06 14:45:28 +0000

    Create a CbAgentSettings class
    Change-Id: I08cf3bf2811177b07549d87a311605be8219f3ff
    Reviewed-on: https://review.couchbase.org/c/perfrunner/+/186220
    Reviewed-by: Laura Silaja <laura.silaja@couchbase.com>
    Reviewed-by: vikas chaudhary <vikas.chaudhary@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- perfrunner/helpers/cbmonitor.py
+++ perfrunner/helpers/cbmonitor.py
@@ -90,84 +90,80 @@ def with_cloudwatch(method, *args, **kwargs):
         logger.info("Cloudwatch unavailable in Python SDK 2 Tests.")
 
 
-def new_cbagent_settings(test: PerfTest):
-    if not test.test_config.stats_settings.enabled:
-        return None
+class CbAgentSettings:
 
-    if hasattr(test, 'ALL_BUCKETS'):
-        buckets = None
-    else:
-        buckets = test.test_config.buckets +\
-            test.test_config.eventing_buckets +\
-            test.test_config.eventing_metadata_bucket
-
-    if hasattr(test, 'ALL_HOSTNAMES'):
-        hostnames = test.cluster_spec.servers
-        if test.cluster_spec.infrastructure_sync_gateways:
-            hostnames += test.cluster_spec.sgw_servers
-    else:
-        hostnames = None
-    settings = type('settings', (object,), {
-        'cbmonitor_host': CBMONITOR_HOST,
-        'interval': test.test_config.stats_settings.interval,
-        'lat_interval': test.test_config.stats_settings.lat_interval,
-        'buckets': buckets,
-        'collections': None,
-        'indexes': {},
-        'hostnames': hostnames,
-        'secondary_statsfile': test.test_config.stats_settings.secondary_statsfile,
-        'client_processes': test.test_config.stats_settings.client_processes,
-        'server_processes': test.test_config.stats_settings.server_processes,
-        'traced_processes': test.test_config.stats_settings.traced_processes,
-        'bucket_username': test.cluster_spec.rest_credentials[0],
-        'bucket_password': test.cluster_spec.rest_credentials[1],
-        'workers': test.cluster_spec.workers,
-        'cloud': {"enabled": False},
-        'serverless_db_names': {},
-        'remote': test.worker_manager.is_remote if test.test_config.test_case.use_workers else False
-    })()
-
-    settings.ssh_username, settings.ssh_password = \
-        test.cluster_spec.ssh_credentials
-    settings.rest_username, settings.rest_password = \
-        test.cluster_spec.rest_credentials
-
-    if test.dynamic_infra:
-        settings.cloud = {"enabled": True, "dynamic": True, "cloud_rest": test.rest}
-    elif test.cluster_spec.cloud_infrastructure:
-        settings.cloud.update({'enabled': True, 'dynamic': False, 'cloud_rest': test.rest})
-        settings.hostnames = test.cluster_spec.servers
-        if test.cluster_spec.serverless_infrastructure:
-            settings.cloud.update({'serverless': True})
-            settings.serverless_db_names = {
-                k: v['name'] for k, v in test.test_config.serverless_db.db_map.items()
-            }
-
-    if test.test_config.collection.collection_map:
-        settings.collections = test.test_config.collection.collection_map
-
-    if test.cluster_spec.servers_by_role('index'):
-        if test.test_config.gsi_settings.indexes:
-            settings.indexes = test.test_config.gsi_settings.indexes
+    def __init__(self, test: PerfTest):
+        if hasattr(test, 'ALL_BUCKETS'):
+            buckets = None
         else:
-            settings.indexes = test.test_config.index_settings.indexes
-        settings.index_node = test.cluster_spec.servers_by_role('index')[0]
-    settings.is_n2n = False
-    if test.test_config.cluster.enable_n2n_encryption is not None:
-        settings.is_n2n = True
+            buckets = test.test_config.buckets +\
+                test.test_config.eventing_buckets +\
+                test.test_config.eventing_metadata_bucket
 
-    if settings.remote:
-        settings.remote_worker_home = test.worker_manager.WORKER_HOME
+        if hasattr(test, 'ALL_HOSTNAMES'):
+            hostnames = test.cluster_spec.servers
+            if test.cluster_spec.infrastructure_sync_gateways:
+                hostnames += test.cluster_spec.sgw_servers
+        else:
+            hostnames = None
 
-    return settings
+        self.cbmonitor_host = CBMONITOR_HOST
+        self.interval = test.test_config.stats_settings.interval
+        self.lat_interval = test.test_config.stats_settings.lat_interval
+        self.buckets = buckets
+        self.collections = None
+        self.indexes = {}
+        self.hostnames = hostnames
+        self.secondary_statsfile = test.test_config.stats_settings.secondary_statsfile
+        self.client_processes = test.test_config.stats_settings.client_processes
+        self.server_processes = test.test_config.stats_settings.server_processes
+        self.traced_processes = test.test_config.stats_settings.traced_processes
+        self.workers = test.cluster_spec.workers
+        self.cloud = {"enabled": False}
+        self.serverless_db_names = {}
+
+        self.remote = False
+        if test.test_config.test_case.use_workers:
+            self.remote = test.worker_manager.is_remote
+
+        self.ssh_username, self.ssh_password = test.cluster_spec.ssh_credentials
+        self.rest_username, self.rest_password = test.cluster_spec.rest_credentials
+        self.bucket_username, self.bucket_password = test.cluster_spec.rest_credentials
+
+        if test.dynamic_infra:
+            self.cloud = {"enabled": True, "dynamic": True, "cloud_rest": test.rest}
+        elif test.cluster_spec.cloud_infrastructure:
+            self.cloud.update({'enabled': True, 'dynamic': False, 'cloud_rest': test.rest})
+            self.hostnames = test.cluster_spec.servers
+            if test.cluster_spec.serverless_infrastructure:
+                self.cloud.update({'serverless': True})
+                self.serverless_db_names = {
+                    k: v['name'] for k, v in test.test_config.serverless_db.db_map.items()
+                }
+
+        if test.test_config.collection.collection_map:
+            self.collections = test.test_config.collection.collection_map
+
+        if test.cluster_spec.servers_by_role('index'):
+            if test.test_config.gsi_settings.indexes:
+                self.indexes = test.test_config.gsi_settings.indexes
+            else:
+                self.indexes = test.test_config.index_settings.indexes
+            self.index_node = test.cluster_spec.servers_by_role('index')[0]
+        self.is_n2n = False
+        if test.test_config.cluster.enable_n2n_encryption is not None:
+            self.is_n2n = True
+
+        if self.remote:
+            self.remote_worker_home = test.worker_manager.WORKER_HOME
 
 
 class CbAgent:
 
     def __init__(self, test: PerfTest, phase: str):
         self.test = test
-        self.settings = new_cbagent_settings(test=test)
         if self.test.test_config.stats_settings.enabled:
+            self.settings = CbAgentSettings(test)
             self.init_clusters(phase=phase)
             self.add_collectors(**test.COLLECTORS)
             self.update_metadata()

734c6ac231d27e0c62704e243625436e1637c0ea
Bo-Chun Wang <bo-chun.wang@couchbase.com>
2023-02-13 10:19:00 -0800

    fix names
    Change-Id: I1859b02d202081d4983705fef1b52b4c122e71ad
    Reviewed-on: https://review.couchbase.org/c/perfrunner/+/186678
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Bo-Chun Wang <bo-chun.wang@couchbase.com>

--- cbagent/collectors/cbstats.py
+++ cbagent/collectors/cbstats.py
@@ -78,10 +78,10 @@ class CBStatsAll(Collector):
         "mem_used_secondary",
         "ep_magma_total_mem_used",
         "ep_magma_mem_used_diff",
-        "ep_data_blocks_uncompressed_size",
-        "ep_data_blocks_compressed_size",
-        "ep_data_blocks_compression_ratio",
-        "ep_data_blocks_space_reduction_estimate_pct"
+        "ep_magma_data_blocks_uncompressed_size",
+        "ep_magma_data_blocks_compressed_size",
+        "ep_magma_data_blocks_compression_ratio",
+        "ep_magma_data_blocks_space_reduction_estimate_pct"
     )
 
     def __init__(self, settings, test):

717c1561d98cd8f9f6b7efa905e8d724b891a49c
Daniel Nagy <daniel.nagy@couchbase.com>
2023-02-14 10:53:11 +0000

    Speed up spring latency data upload
    Change-Id: I41e9c9be8635aece9e97727f7d66e90e4f43357e
    Reviewed-on: https://review.couchbase.org/c/perfrunner/+/186718
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

--- cbagent/collectors/latency.py
+++ cbagent/collectors/latency.py
@@ -3,7 +3,7 @@ import csv
 import glob
 from typing import Iterator
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, TCPConnector
 from fabric.api import cd, execute, get, parallel, run
 
 from cbagent.collectors.collector import Collector
@@ -47,21 +47,27 @@ class KVLatency(Latency):
 
         execute(parallel(task), hosts=self.workers)
 
-    def read_stats(self, bucket: str) -> Iterator:
-        for filename in glob.glob(self.PATTERN + bucket):
-            with open(filename) as fh:
-                reader = csv.reader(fh)
-                for line in reader:
-                    yield line
+    def read_stats(self, filename: str) -> Iterator:
+        with open(filename) as fh:
+            reader = csv.reader(fh)
+            for line in reader:
+                yield line
 
-    async def post_results(self, bucket: str):
-        async with ClientSession() as self.store.async_session:
-            for line in self.read_stats(bucket):
-                operation = line[0]
-                timestamp = line[1]
-                latency = line[2]
+    async def post_results(self, filename: str, bucket: str):
+        for line in self.read_stats(filename):
+            operation, timestamp, latency = line[:3]
+            data = {
+                'latency_' + operation: float(latency) * 1000,  # Latency in ms
+            }
+            await self.append_to_store_async(data=data,
+                                             timestamp=int(timestamp),
+                                             cluster=self.cluster,
+                                             bucket=bucket,
+                                             collector=self.COLLECTOR)
+            if len(line) == 4:
+                total_latency = line[3]
                 data = {
-                    'latency_' + operation: float(latency) * 1000,  # Latency in ms
+                    'latency_total_' + operation: float(total_latency) * 1000,  # Latency in ms
                 }
                 await self.append_to_store_async(data=data,
                                                  timestamp=int(timestamp),
@@ -69,16 +75,13 @@ class KVLatency(Latency):
                                                  bucket=bucket,
                                                  collector=self.COLLECTOR)
 
-                if len(line) == 4:
-                    total_latency = line[3]
-                    data = {
-                        'latency_total_' + operation: float(total_latency) * 1000,  # Latency in ms
-                    }
-                    await self.append_to_store_async(data=data,
-                                                     timestamp=int(timestamp),
-                                                     cluster=self.cluster,
-                                                     bucket=bucket,
-                                                     collector=self.COLLECTOR)
+    async def post_all_results(self):
+        async with ClientSession(connector=TCPConnector()) as self.store.async_session:
+            await asyncio.gather(*[
+                self.post_results(fn, bucket)
+                for bucket in self.get_buckets()
+                for fn in glob.glob(self.PATTERN + bucket)
+            ])
 
     def reconstruct(self):
         if self.remote_workers:
@@ -88,8 +91,7 @@ class KVLatency(Latency):
         if (loop := asyncio.get_event_loop()).is_closed():
             loop = asyncio.new_event_loop()
 
-        for bucket in self.get_buckets():
-            loop.run_until_complete(self.post_results(bucket))
+        loop.run_until_complete(self.post_all_results())
 
         loop.close()
 

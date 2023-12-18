3a502416b3ab0866d6501c79b812cf4b5d649f29
devanshsa5 <devansh.srivastava@couchbase.com>
2023-12-18 20:07:38 +0530

    Updating the assume-cpcpli-perfrunner role
    Change-Id: I422471b4a687dd88b97e839c491a0f32cf40568b
    Reviewed-on: https://review.couchbase.org/c/perfrunner/+/202740
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Daniel Nagy <daniel.nagy@couchbase.com>

--- scripts/assume_cp_cli_perfrunner_role.sh
+++ scripts/assume_cp_cli_perfrunner_role.sh
@@ -8,7 +8,7 @@ fi
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 
-AWS_ASSUMED_ROLE=$(aws sts assume-role --role-arn "arn:aws:iam::264138468394:role/_cp-cli-perfrunner" --role-session-name "perfrunner" --duration-seconds 43200)
+AWS_ASSUMED_ROLE=$(env/bin/aws sts assume-role --role-arn "arn:aws:iam::264138468394:role/_cp-cli-perfrunner" --role-session-name "perfrunner" --duration-seconds 43200)
 export AWS_ACCESS_KEY_ID=$(echo "${AWS_ASSUMED_ROLE}" | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo "${AWS_ASSUMED_ROLE}" | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo "${AWS_ASSUMED_ROLE}" | jq -r .Credentials.SessionToken)

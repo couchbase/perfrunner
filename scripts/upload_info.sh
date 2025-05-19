05fb012a71edb2c820adda1cb65ff252d20db68a
Salim Salim <salim.salim@couchbase.com>
2025-05-19 19:21:53 +0100

    k8s: Include cao collected logs as part of upload process when present
    Also clean up the script and make it into a reusable function.
    
    Change-Id: I387d79160b8f73f112449794e00e5c2bc83fa4b5
    Reviewed-on: https://review.couchbase.org/c/perfrunner/+/227994
    Reviewed-by: Elliot Broxton <elliot.broxton@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- scripts/upload_info.sh
+++ scripts/upload_info.sh
@@ -1,13 +1,15 @@
 #!/bin/bash +x
+# Upload files matching the pattern to S3 bucket
+upload_files_to_s3() {
+    local pattern=$1
 
-for FILE in *.zip; do
-    [ -f "$FILE" ] || continue
-    env/bin/aws s3 cp --quiet ${FILE} s3://perf-artifacts/${BUILD_TAG}/${FILE}
-    echo "https://s3-us-west-2.amazonaws.com/perf-artifacts/${BUILD_TAG}/${FILE}"
-done
+    for FILE in $pattern; do
+        [ -f "$FILE" ] || continue
+        env/bin/aws s3 cp --quiet ${FILE} s3://perf-artifacts/${BUILD_TAG}/${FILE}
+        echo "https://s3-us-west-2.amazonaws.com/perf-artifacts/${BUILD_TAG}/${FILE}"
+    done
+}
 
-for FILE in *.prof; do
-    [ -f "$FILE" ] || continue
-    env/bin/aws s3 cp --quiet ${FILE} s3://perf-artifacts/${BUILD_TAG}/${FILE}
-    echo "https://s3-us-west-2.amazonaws.com/perf-artifacts/${BUILD_TAG}/${FILE}"
-done
+# Upload zip and prof files
+# cbcollect zip files, profile files, and CAO collected logs
+upload_files_to_s3 "*.zip *.prof cbopinfo-*.tar.gz"
